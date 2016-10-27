059fe161978b3da0f161610b9b824b42d42977f6
mitchellsuter <mitchellasuter@gmail.com>
2016-10-27 11:25:36 -0400

    MOD: update strategies script and updated statistics routines

--- ram/repository/update_strats.py
+++ ram/repository/update_strats.py
@@ -1,10 +1,12 @@
-
 import os
 import importlib
 
+from ram.utils.statistics import create_strategy_report
+
+OUTDIR = os.path.join(os.getenv('DATA'), 'ram', 'strategy_output')
 
 # Committed Strategies - Move to config?
-#   {'folder': 'strategyclass'}
+#   ('folder', 'strategyclass')
 
 STRATS = [
     ('vxx', 'VXXStrategy'),
@@ -12,13 +14,24 @@ STRATS = [
     ('statarb', 'StatArbStrategy')
 ]
 
+print 'Updating strategies...'
 
 for strat in STRATS:
+
     mdir = strat[0]
     mclass = strat[1]
     module = importlib.import_module('ram.strategy.{0}.main'.format(mdir))
+
     Strategy = getattr(module, mclass)
 
     strat = Strategy()
-    strat.start_live()
+    strat.start()
     results = strat.get_results()
+
+    # Write results
+    results.to_csv(os.path.join(OUTDIR, '{0}_returns.csv'.format(mclass)))
+
+    # Stats
+    create_strategy_report(results, mclass, OUTDIR)
+
+    print mclass
--- ram/utils/statistics.py
+++ ram/utils/statistics.py
@@ -17,41 +17,80 @@ def create_strategy_report(results, name, path):
     results = results.astype(float)
 
     # STATISTICS
-    _get_stats(results).to_csv(os.path.join(path, 'results_{0}.csv'.format(name)))
-    _get_stats(results.loc[dt.datetime(2012, 1, 1):]).to_csv(
-        os.path.join(path, 'results_{0}_since2012.csv'.format(name)))
+    stats1 = get_stats(results)
+    stats2 = get_stats(results.loc[dt.datetime(2012, 1, 1):])
+
+    stats1.to_csv(os.path.join(path, '{0}_stats.csv'.format(name)))
+    stats2.to_csv(os.path.join(path, '{0}_stats_since2012.csv'.format(name)))
 
     # PLOTTING
-    _make_plot(results, name, path)
+    make_plot(results, name, path)
 
 
-def _get_stats(results):
+###############################################################################
+
+def get_stats(results):
+    """
+    `results` is a data frame of returns, not cum returns or PL
+    """
     out = pd.DataFrame(columns=[
         'Total', 'Mean', 'Std', 'Skew', 'Kurt',
         'Sharpe', 'Sortino', 'MinRet', 'WinP'])
 
     # TOTAL
-    out.loc[:, 'Total'] = results.sum()
+    out.loc[:, 'Total'] = results.sum().round(4)
 
     # MOMENTS
-    out.loc[:, 'Mean'] = results.mean()
-    out.loc[:, 'Std'] = results.std()
-    out.loc[:, 'Skew'] = results.skew()
-    out.loc[:, 'Kurt'] = results.kurt()
+    out.loc[:, 'Mean'] = results.mean().round(4)
+    out.loc[:, 'Std'] = results.std().round(4)
+    out.loc[:, 'Skew'] = results.skew().round(4)
+    out.loc[:, 'Kurt'] = results.kurt().round(2)
 
     # FINANCE STATS
-    out.loc[:, 'Sharpe'] = results.mean() / results.std() * np.sqrt(252)
-    out.loc[:, 'Sortino'] = results.mean() / \
-        results.where(results < 0).fillna(0).std() * np.sqrt(252)
-    out.loc[:, 'MinRet'] = results.min()
-    out.loc[:, 'WinP'] = (results > 0).mean()
+    out.loc[:, 'Sharpe'] = (results.mean() /
+                            results.std() * np.sqrt(252)).round(4)
+    out.loc[:, 'Sortino'] = (results.mean() /
+        results.where(results < 0).fillna(0).std() * np.sqrt(252)).round(4)
+    out.loc[:, 'MinRet'] = results.min().round(4)
+    out.loc[:, 'WinP'] = (results > 0).mean().round(4)
 
     # DRAWDOWNS
     dd = _get_drawdowns(results)
     out = out.join(dd)
 
+    # TIME AT HIGHS
+    highs = _time_at_highs(results)
+    out = out.join(highs)
+
+    # PSEUDO VAR MEASURES
+    var1 = _value_at_risk(results)
+    out = out.join(var1)
+    var2 = _c_value_at_risk(results)
+    out = out.join(var2)
+    var3 = _lower_partial_moment(results)
+    out = out.join(var3)
+
     return out.T
 
+
+def make_plot(results, name=None, spath=None):
+    data = results.copy()
+    data = data.cumsum()
+
+    plt.figure()
+    for col, vals in data.iteritems():
+        plt.plot(vals, label=col)
+    plt.grid()
+    if data.shape[1] < 6:
+        plt.legend(loc=2)
+    if name:
+        plt.savefig('{0}/{1}'.format(spath, '{0}_plot'.format(name)))
+    else:
+        plt.show()
+
+
+###############################################################################
+#  Stats implementations
 
 def _get_drawdowns(df):
 
@@ -77,27 +116,15 @@ def _get_drawdowns(df):
     return outdf
 
 
-def _make_plot(data, name, spath):
-    data = data.copy()
-    data = data.cumsum()
-
-    plt.figure()
-    for col, vals in data.iteritems():
-        plt.plot(vals, label=col)
-    plt.grid()
-    if data.shape[1] < 6:
-        plt.legend(loc=2)
-    plt.savefig('{0}/{1}'.format(spath, 'results_plot_{0}'.format(name)))
+def _time_at_highs(df):
+    df = df.copy()
+    df = df.cumsum()
+    highs = np.maximum.accumulate(df)
+    out = pd.DataFrame({'TimeAtHighs': (df == highs).sum() / len(df)})
+    return out.round(3)
 
 
-
-def get_time_at_highs(cumpl):
-    highs = np.maximum.accumulate(cumpl)
-    return pd.Series((cumpl == highs).sum() / float(len(cumpl)),
-                     name='TimeAtHighs')
-
-
-def Xbeta(returns, market):
+def _beta(returns, market):
     # Create a matrix of [returns, market]
     m = np.matrix([returns, market])
     # Return the covariance of m divided by the standard deviation
@@ -105,36 +132,41 @@ def Xbeta(returns, market):
     return np.cov(m)[0][1] / np.std(market)
 
 
-def value_at_risk(returns, alpha=0.05):
+def _value_at_risk(returns):
     """
     What is the worst probable loss in a (1-alpha) situation
     """
     # Calculate the index associated with alpha
+    alpha = 0.05
     index = int(alpha * len(returns))
     # Iterate through each column
-    out = pd.Series(index=returns.columns, name='VaR_5perc')
+    out = pd.DataFrame(index=returns.columns, columns=['VaR_5perc'])
     for column in returns:
-        out[column] = np.sort(returns[column])[index]
+        out.loc[column, 'VaR_5perc'] = np.sort(returns[column])[index]
     # VaR is positive in literature but I like looking at it as neg
+    out = out.astype(float).round(4)
     return out
 
 
-def c_value_at_risk(returns, alpha=0.05):
+def _c_value_at_risk(returns):
     """
     Conditional VaR is the average of all the losses beyond the 0.05 VaR
     loss.
     """
     # Calculate the index associated with alpha
+    alpha = 0.05
     index = int(alpha * len(returns))
     # Iterate through each column
-    out = pd.Series(index=returns.columns, name='CVaR_5perc')
+    out = pd.DataFrame(index=returns.columns, columns=['CVaR_5perc'])
     for column in returns:
-        out[column] = np.mean(np.sort(returns[column])[:index])
+        out.loc[column, 'CVaR_5perc'] = np.mean(np.sort(
+            returns[column])[:index])
     # VaR is positive in literature but I like looking at it as neg
+    out = out.astype(float).round(4)
     return out
 
 
-def lower_partial_moment(returns, threshold=0, order=1):
+def _lower_partial_moment(returns):
     """
     Lower partial moment: Whereas measures of risk-adjusted return
     based on volatility treat all deviations from the mean as risk,
@@ -149,12 +181,12 @@ def lower_partial_moment(returns, threshold=0, order=1):
     below the target threshold, meaning that larger orders result in
     more risk-averse measures.
     """
+    threshold = 0
+    order = 2
     # Get spreads between expectation and actual. Flip sign to represent
     # this risk number as positive being bad
     diff = -(returns-threshold)
     diff = diff * (diff > 0)
     diff = diff ** order
-    out = diff.mean()
-    out.name = 'LPM_1'
+    out = pd.DataFrame({'LPM_2': diff.mean() * 1000}).astype(float).round(4)
     return out
-
