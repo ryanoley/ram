4fdcb217880bab526de320497f73e7233c33905e
mitchellsuter <mitchellasuter@gmail.com>
2016-11-07 16:39:45 -0500

    MOD: routine commit for sql features

--- ram/data/sql_features.py
+++ ram/data/sql_features.py
@@ -1,235 +1,171 @@
-
-# Allowable columns
-MASTER_COLS = ['Open_', 'High', 'Low', 'Close_',
-               'Vwap', 'Volume', 'AvgDolVol', 'MarketCap',
-               'CashDividend', 'SplitFactor']
+import re
 
 
 def sqlcmd_from_feature_list(features):
-    """
-    Function facing the data handler. Returns the features that are
-    able to be queried, and the sql command to get those columns.
-    """
-    if not isinstance(features, list):
-        features = [features]
-
-    cte_out = []
-    body_out = []
-    feature_out = []
+    # Get individual entries for CTEs per feature
+    ctes = []
     for f in features:
-        f2 = _get_feature_string(f)
-        if f2:
-            feature_out.append(f)
-            cte_out.append(f2[0])
-            body_out.append(f2[1])
-    return feature_out, ', '.join(cte_out), ', '.join(body_out)
+        ctes.append(make_cmds(f))
+
+    cte1, cte2, cte3 = zip(*ctes)
+    vars1 = ','.join(cte1)
+    vars2 = ','.join(cte2)
+    vars3 = ','.join(cte3)
+
+    sqlcmd = \
+    """
+    ; with cte1 as (
+        select {0}
+        from ram.dbo.ram_master_equities
+    )
+    , cte2 as (
+        select {1}
+        from cte1
+    )
+    , cte3 as (
+        select {2}
+        from cte2
+    )
+    select * from cte3
+    """.format(vars1, vars2, vars3)
+    return clean_sql_cmd(sqlcmd)
 
 
-def _get_feature_string(feature):
-    # Split features
-    sp = feature.split('_')
-    if sp[0] == 'PRMA':
-        return _prma(feature)
-    elif sp[0] == 'MA':
-        return _ma(feature)
-    elif sp[0] == 'LAGMA':
-        return _lag_ma(feature)
-    elif sp[0] == 'BOLL':
-        return _bollinger(feature)
-    elif sp[0] == 'ADJ':
-        return _adjust_price(feature)
-    elif sp[0] == 'LAGADJ':
-        return _lag_adj_price(feature)
-    elif sp[0] == 'VOL':
-        return _vol(feature)
-    elif sp[0] == 'LAGVOL':
-        return _vol(feature, True)
-    elif sp[0] == 'P':
-        return _actual_price(feature)
-    else:
-        pass
+def make_cmds(vstring):
+    params = parse_input_var(vstring)
+    # Call function that corresponds to user input. Will handle
+    # if there is no manipulation for a variable, aka just return
+    # data from the table.
+    cte1, cte2 = globals()[params['var'][0]](params)
+    cte3 = globals()[params['manip'][0]](params)
+    return clean_sql_cmd(cte1), clean_sql_cmd(cte2), clean_sql_cmd(cte3)
 
 
-###############################################################################
-# Technical variables - MUST RETURN TWO STRINGS FOR TWO SEQUENTIAL QUERIES
+def parse_input_var(vstring):
+    """
+    Takes individual Feature and parses into dictionary used downstream.
 
-def _prma(feature):
-    sp = feature.split('_')
-    days = int(sp[1])
-    col = sp[2]
-    if col in ['Open', 'Close']:
-        col = col + '_'
-    prma_string = \
+    The format should be something like:
+        LAG1_PRMA10_Close
+    """
+    args = re.split('(\d+)_', vstring)
+    out = {'name': vstring}
+
+    while args:
+
+        # Manipulations
+        if args[0] in ['LAG', 'LEAD', 'RANK']:
+            out['manip'] = (args[0], int(args[1]))
+            args = args[2:]
+
+        # Variables
+        elif args[0] in ['MA', 'PRMA', 'VOL', 'BOLL']:
+            out['var'] = (args[0], int(args[1]))
+            args = args[2:]
+
+        # Adjustment irrelevant columns
+        elif args[0] in ['AvgDolVol', 'MarketCap', 'SplitFactor']:
+            out['datacol'] = args[0]
+            break
+
+        # Adjusted data
+        elif args[0] in ['Open', 'High', 'Low', 'Close', 'Vwap', 'Volume']:
+            out['datacol'] = 'Adj' + args[0]
+            break
+
+        # Raw data
+        elif args[0][0] == 'R':
+            col = args[0][1:]
+            assert col in ['Open', 'High', 'Low', 'Close', 'Vwap',
+                           'Volume', 'CashDividend']
+            if col in ['Open', 'Close']:
+                col += '_'
+            out['datacol'] = col
+            break
+
+        else:
+            raise Exception('Input not properly formatted')
+
+    if 'var' not in out:
+        out['var'] = ('pass_through_var', 0)
+
+    if 'manip' not in out:
+        out['manip'] = ('pass_through_manip', 0)
+
+    return out
+
+
+def clean_sql_cmd(sqlcmd):
+    return sqlcmd.replace('  ', '').replace('\n', ' ')
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def LAG(params):
+    name = params['name']
+    periods = params['manip'][1]
+
+    sqlcmd = \
         """
-        {0}/{1} as {2}
-        """.format(_adj(col), _rolling_avg(col, int(days)), feature)
-    return prma_string, feature
+        lag({0}, {1}) over (
+            partition by IdcCode
+            order by Date_) as {0}
+        """.format(name, periods)
+    return sqlcmd
 
 
-def _ma(feature):
-    sp = feature.split('_')
-    days = int(sp[1])
-    col = sp[2]
-    if col in ['Open', 'Close']:
-        col = col + '_'
-    ma_string = \
+def LEAD(params):
+    name = params['name']
+    periods = params['manip'][1]
+
+    sqlcmd = \
+        """
+        lead({0}, {1}) over (
+            partition by IdcCode
+            order by Date_) as {0}
+        """.format(name, periods)
+    return sqlcmd
+
+
+def pass_through_manip(params):
+    name = params['name']
+    sqlcmd = \
+        """
+        {0}
+        """.format(name)
+    return sqlcmd
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#  All variable functions need to return TWO commands for CTEs
+
+def pass_through_var(params):
+    name = params['name']
+    column = params['datacol']
+    sqlcmd1 = \
         """
         {0} as {1}
-        """.format(_rolling_avg(col, int(days)), feature)
-    return ma_string, feature
-
-
-def _lag_ma(feature):
-    sp = feature.split('_')
-    days = int(sp[1])
-    col = sp[2]
-    if col in ['Open', 'Close']:
-        col = col + '_'
-    # Part I: Moving Average
-    ma_string = \
+        """.format(column, name)
+    sqlcmd2 = \
         """
-        {0} as {1}
-        """.format(_rolling_avg(col, int(days)), feature+'temp')
-    # Part II: Lag MA
-    lag_string = \
+        {0}
+        """.format(name)
+    return sqlcmd1, sqlcmd2
+
+
+def PRMA(params):
+    column = params['datacol']
+    length = params['var'][1]
+    name = params['name']
+    sqlcmd1 = \
         """
-        {0} as {1}
-        """.format(_lag(feature+'temp', 1), feature)
-
-    return ma_string, lag_string
-
-
-def _bollinger(feature):
-    """
-    Re-write this to be clearer.
-    """
-    sp = feature.split('_')
-    days = int(sp[1])
-    col = sp[2]
-    # Adjustment for Close/Open columns
-    if col in ['Open', 'Close']:
-        col = col + '_'
-    # Get the high and low side of the for the bollinger band
-    lowstr = "({0} - 2 * {1})".format(
-        _rolling_avg(col, days), _rolling_std(col, days))
-    highstr = "({0} + 2 * {1})".format(
-        _rolling_avg(col, days), _rolling_std(col, days))
-    bollinger_string = \
-        """
-        ({0} - {1}) / nullif({2} - {1}, 0) as {3}
-        """.format(_adj(col), lowstr, highstr, feature)
-    return bollinger_string, feature
-
-
-def _vol(feature, lag=False):
-    sp = feature.split('_')
-    days = int(sp[1])
-    col = sp[2]
-    # Adjustment for Close/Open columns
-    if col in ['Open', 'Close']:
-        col = col + '_'
-    # PART 1 - For CTE
-    # PART 2 - For query
-    if lag:
-        vol_string = \
-            """
-            {1} as {3}LAGVOL{0},
-            {2} as Lag{3}LAGVOL{0}
-            """.format(days, _adj(col), _lag(_adj(col), 1), col)
-        vol_string2 = \
-            """
-            stdev({3}LAGVOL{0} / Lag{3}LAGVOL{0}) over (
-                partition by IdcCode
-                order by Date_
-                rows between {1} preceding and 1 preceding) as {2}
-            """.format(days, days, feature, col)
-    else:
-        vol_string = \
-            """
-            {1} as {3}VOL{0},
-            {2} as Lag{3}VOL{0}
-            """.format(days, _adj(col), _lag(_adj(col), 1), col)
-        vol_string2 = \
-            """
-            stdev({3}VOL{0} / Lag{3}VOL{0}) over (
-                partition by IdcCode
-                order by Date_
-                rows between {1} preceding and current row) as {2}
-            """.format(days, days - 1, feature, col)
-    return vol_string, vol_string2
-
-
-def _adjust_price(feature):
-    sp = feature.split('_')
-    col = sp[1]
-    # Adjustment for Close/Open columns
-    if col in ['Open', 'Close']:
-        col = col + '_'
-    return "{0} as {1} ".format(_adj(col), feature), feature
-
-
-def _lag_adj_price(feature):
-    sp = feature.split('_')
-    days = int(sp[1])
-    col = sp[2]
-    # Adjustment for Close/Open columns
-    if col in ['Open', 'Close']:
-        col = col + '_'
-    return "{0} as {1} ".format(_lag(_adj(col), days), feature), feature
-
-
-def _actual_price(feature):
-    sp = feature.split('_')
-    col = sp[1]
-    # Adjustment for Close/Open columns
-    if col in ['Open', 'Close']:
-        col = col + '_'
-    return "{0} as {1} ".format(col, feature), feature
-
-
-###############################################################################
-# HELPERS
-
-def _adj(feature):
-    assert feature in ['Open_', 'High', 'Low', 'Close_', 'Vwap']
-    return "({0} * DividendFactor * SplitFactor)".format(feature)
-
-
-def _rolling_avg(col, days):
-    """
-    Will always adjust prices because of the time component
-    """
-    offset = days - 1
-    avg_string = \
-        """
-        (avg({0}) over
-            (partition by IdcCode
-             order by Date_
-             rows between {1} preceding and current row))
-        """.format(_adj(col), offset)
-    return avg_string
-
-
-def _rolling_std(col, days):
-    """
-    Will always adjust prices because of the time component
-    """
-    offset = days - 1
-    std_string = \
-        """
-        (stdev({0}) over (
+        {0} / avg({0}) over (
             partition by IdcCode
             order by Date_
-            rows between {1} preceding and current row))
-        """.format(_adj(col), offset)
-    return std_string
-
-
-def _lag(col, offset):
-    lag_string = \
+            rows between {1} preceding and current row) as {2}
+        """.format(column, length-1, name)
+    sqlcmd2 = \
         """
-        (Lag({0}, {1}) over (
-            partition by IdcCode
-            order by Date_))
-        """.format(col, offset)
-    return lag_string
+        {0}
+        """.format(name)
+    return sqlcmd1, sqlcmd2
--- ram/tests/test_data_sql_features.py
+++ ram/tests/test_data_sql_features.py
@@ -1,5 +1,4 @@
 import unittest
-import numpy as np
 
 import ram.data.sql_features as sqlf
 
@@ -9,175 +8,45 @@ class TestSqlFeatures(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_actual_price(self):
-        result = sqlf._actual_price('P_Open')
-        benchmark = 'Open_ as P_Open'
-        self.assertEqual(clean(result[0]), clean(benchmark))
-        self.assertEqual(clean(result[1]), 'P_Open')
-
-    def test_vol(self):
-        result = sqlf._vol('VOL_30_Open')
-        benchmark = """
-            (Open_ * DividendFactor * SplitFactor) as Open_VOL30,
-            (Lag((Open_ * DividendFactor * SplitFactor), 1) over (
-                partition by IdcCode
-                order by Date_))
-            as LagOpen_VOL30
-        """
-        self.assertEqual(clean(result[0]), clean(benchmark))
-        benchmark = """
-            stdev(Open_VOL30 / LagOpen_VOL30) over (
-            partition by IdcCode
-            order by Date_
-            rows between 29 preceding and current row) as VOL_30_Open
-        """
-        self.assertEqual(clean(result[1]), clean(benchmark))
-
-    def test_lag(self):
-        result = sqlf._lag('Close_', 2)
-        benchmark = '(Lag(Close_, 2) ' + \
-                    'over (partition by IdcCode order by Date_))'
-        self.assertEqual(clean(result), clean(benchmark))
-
-    def test_lag_adj_price(self):
-        result = sqlf._lag_adj_price('LAGADJ_2_Close')
-        benchmark = '(Lag( (Close_ * DividendFactor * SplitFactor) , 2) ' + \
-                    'over (partition by IdcCode order by Date_)) ' + \
-                    'as LAGADJ_2_Close'
-        self.assertEqual(clean(result[0]), clean(benchmark))
-        self.assertEqual(clean(result[1]), 'LAGADJ_2_Close')
-
-    def test_rolling_avg(self):
-        result = sqlf._rolling_avg('High', 10)
-        benchmark = '(avg((High * DividendFactor * SplitFactor)) over ' + \
-                    '(partition by IdcCode order by Date_ rows between ' + \
-                    '9 preceding and current row)) '
-        self.assertEqual(clean(result), clean(benchmark))
-
-    def test_rolling_std(self):
-        result = sqlf._rolling_std('Close_', 19)
-        benchmark = '(stdev((Close_ * DividendFactor * SplitFactor)) over ' + \
-                    '(partition by IdcCode order by Date_ rows between ' + \
-                    '18 preceding and current row)) '
-        self.assertEqual(clean(result), clean(benchmark))
-
-    def test_adjust_price(self):
-        result = sqlf._adjust_price('ADJ_Close')
-        benchmark = ' (Close_ * DividendFactor * SplitFactor)  as ADJ_Close'
-        self.assertEqual(clean(result[0]), clean(benchmark))
-        self.assertEqual(clean(result[1]), 'ADJ_Close')
-
-    def test_adj(self):
-        result = sqlf._adj('Close_')
-        benchmark = '(Close_ * DividendFactor * SplitFactor)'
-        self.assertEqual(result, benchmark)
-
-    def test_prma(self):
-        feature = 'PRMA_10_Close'
-        result = sqlf._prma(feature)
-        benchmark = ' (Close_ * DividendFactor * SplitFactor) / ' + \
-                    '(avg((Close_ * DividendFactor * SplitFactor)) over ' + \
-                    '(partition by IdcCode order by Date_ rows between 9 ' + \
-                    'preceding and current row)) as PRMA_10_Close'
-        self.assertEqual(clean(result[0]), clean(benchmark))
-        self.assertEqual(clean(result[1]), 'PRMA_10_Close')
-        #
-        result = sqlf._prma('PRMA_39_Open')
-        benchmark = ' (Open_ * DividendFactor * SplitFactor) / ' + \
-                    '(avg((Open_ * DividendFactor * SplitFactor)) over ' + \
-                    '(partition by IdcCode order by Date_ rows between 38 ' + \
-                    'preceding and current row)) as PRMA_39_Open'
-        self.assertEqual(clean(result[0]), clean(benchmark))
-        self.assertEqual(clean(result[1]), 'PRMA_39_Open')
-
-    def test_ma(self):
-        feature = 'MA_10_Close'
-        result = sqlf._ma(feature)
-        benchmark = '(avg((Close_ * DividendFactor * SplitFactor)) over ' + \
-                    '(partition by IdcCode order by Date_ rows between 9 ' + \
-                    'preceding and current row)) as MA_10_Close'
-        self.assertEqual(clean(result[0]), clean(benchmark))
-        self.assertEqual(clean(result[1]), 'MA_10_Close')
-
-    def test_lag_ma(self):
-        feature = 'LAGMA_10_Close'
-        result = sqlf._lag_ma(feature)
-        benchmark = """
-            (avg((Close_ * DividendFactor * SplitFactor)) over
-            (partition by IdcCode
-            order by Date_
-            rows between 9 preceding and current row))
-            as LAGMA_10_Closetemp
-        """
-        self.assertEqual(clean(result[0]), clean(benchmark))
-        benchmark = """
-            (Lag(LAGMA_10_Closetemp, 1) over (
-            partition by IdcCode
-            order by Date_))
-            as LAGMA_10_Close
-        """
-        self.assertEqual(clean(result[1]), clean(benchmark))
-
-    def test_bollinger(self):
-        result = sqlf._bollinger('BOLL_20_Close')
-        benchmark = \
-            '( (Close_ * DividendFactor * SplitFactor) - ' + \
-            '( (avg((Close_ * DividendFactor * SplitFactor)) over ' + \
-            '(partition by IdcCode order by Date_ rows between 19 ' + \
-            'preceding and current row)) - 2 * (stdev((Close_ * ' + \
-            'DividendFactor * SplitFactor)) over (partition by IdcCode ' + \
-            'order by Date_ rows between 19 preceding and current row)) ' + \
-            ')) / nullif(((avg((Close_ * DividendFactor * SplitFactor))' + \
-            'over (partition by IdcCode order by Date_ rows between 19 ' + \
-            'preceding and current row)) + 2 * (stdev((Close_ * ' + \
-            'DividendFactor * SplitFactor)) over (partition by IdcCode ' + \
-            'order by Date_ rows between 19 preceding and current row)) ' + \
-            ') - ( (avg((Close_ * DividendFactor * SplitFactor)) over ' + \
-            '(partition by IdcCode order by Date_ rows between 19 ' + \
-            'preceding and current row)) - 2 * (stdev((Close_ * ' + \
-            'DividendFactor * SplitFactor)) over (partition by IdcCode ' + \
-            'order by Date_ rows between 19 preceding and current row)' + \
-            ')), 0) as BOLL_20_Close'
-        self.assertEqual(clean(result[0]), clean(benchmark))
-        self.assertEqual(clean(result[1]), 'BOLL_20_Close')
-        # High
-        result = sqlf._bollinger('BOLL_20_High')
-        benchmark = \
-            '( (High * DividendFactor * SplitFactor) - ' + \
-            '( (avg((High * DividendFactor * SplitFactor)) over ' + \
-            '(partition by IdcCode order by Date_ rows between 19 ' + \
-            'preceding and current row)) - 2 * (stdev((High * ' + \
-            'DividendFactor * SplitFactor)) over (partition by IdcCode ' + \
-            'order by Date_ rows between 19 preceding and current row)) ' + \
-            ')) / nullif(((avg((High * DividendFactor * SplitFactor))' + \
-            'over (partition by IdcCode order by Date_ rows between 19 ' + \
-            'preceding and current row)) + 2 * (stdev((High * ' + \
-            'DividendFactor * SplitFactor)) over (partition by IdcCode ' + \
-            'order by Date_ rows between 19 preceding and current row)) ' + \
-            ') - ( (avg((High * DividendFactor * SplitFactor)) over ' + \
-            '(partition by IdcCode order by Date_ rows between 19 ' + \
-            'preceding and current row)) - 2 * (stdev((High * ' + \
-            'DividendFactor * SplitFactor)) over (partition by IdcCode ' + \
-            'order by Date_ rows between 19 preceding and current row)' + \
-            ')), 0) as BOLL_20_High'
-        self.assertEqual(clean(result[0]), clean(benchmark))
-        self.assertEqual(clean(result[1]), 'BOLL_20_High')
-
     def test_sqlcmd_from_feature_list(self):
-        features = ['PRMA_10_Close', 'BOLL_20_Open', 'P_Close']
+        features = ['AvgDolVol', 'LAG1_RClose', 'LEAD1_PRMA10_High']
         result = sqlf.sqlcmd_from_feature_list(features)
-        self.assertListEqual(result[0], features)
-        # No test written
-        features = ['PRMA_10_Close', 'P_Close', 'AS@*%&$@']
-        result = sqlf.sqlcmd_from_feature_list(features)
-        self.assertListEqual(result[0], ['PRMA_10_Close', 'P_Close'])
+
+    def test_parse_input_var(self):
+        result = sqlf.parse_input_var('LAG1_ROpen')
+        benchmark = {'name': 'LAG1_ROpen', 'datacol': 'Open_',
+                     'manip': ('LAG', 1), 'var': ('pass_through_var', 0)}
+        self.assertDictEqual(result, benchmark)
+        result = sqlf.parse_input_var('LEAD1_PRMA20_Open')
+        benchmark = {'name': 'LEAD1_PRMA20_Open', 'datacol': 'AdjOpen',
+                     'manip': ('LEAD', 1), 'var': ('PRMA', 20)}
+        self.assertDictEqual(result, benchmark)
+        result = sqlf.parse_input_var('AvgDolVol')
+        benchmark = {'name': 'AvgDolVol', 'datacol': 'AvgDolVol',
+                     'var': ('pass_through_var', 0),
+                     'manip': ('pass_through_manip', 0)}
+        self.assertDictEqual(result, benchmark)
+        # Test that these fail
+        self.assertRaises(Exception, sqlf.parse_input_var, 'XERB')
+        self.assertRaises(Exception, sqlf.parse_input_var, 'Lag1_Open')
+        self.assertRaises(Exception, sqlf.parse_input_var, 'OPEN')
+
+    def test_make_cmds(self):
+        vstring = 'LAG1_PRMA10_Close'
+        result = sqlf.make_cmds(vstring)
+        self.assertEqual(
+            result[0],
+            ' AdjClose / avg(AdjClose) over ( partition by IdcCode '
+            'order by Date_ rows between 9 preceding and current row) '
+            'as LAG1_PRMA10_Close ')
+        self.assertEqual(result[1], ' LAG1_PRMA10_Close ')
+        self.assertEqual(
+            result[2],
+            ' lag(LAG1_PRMA10_Close, 1) over ( partition by IdcCode '
+            'order by Date_) as LAG1_PRMA10_Close ')
 
     def tearDown(self):
         pass
-
-
-def clean(string):
-    return string.replace('\n', '').replace(' ', '')
 
 
 if __name__ == '__main__':
