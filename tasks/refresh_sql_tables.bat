ad0efc928ad291237de7cabfd221a7b774603fc5
RAMServer <analysts@roundaboutam.com>
2016-11-11 15:10:55 -0500

    BUG: task had incorrect path for etf table

--- tasks/refresh_sql_tables.bat
+++ tasks/refresh_sql_tables.bat
@@ -4,4 +4,4 @@ set RAMSCRIPTDIR=%GITHUB%\ram\ram\data\qad
 set SQLCMDAUTH="%SQLCMDPATH%\sqlcmd" -S 192.168.2.8 -d ram -U ramuser -P 183madison
 
 %SQLCMDAUTH% -i "%RAMSCRIPTDIR%\master_equities.sql"
-%SQLCMDAUTH% -i "%RAMSCRIPTDIR%\master_etfs.sql"
+%SQLCMDAUTH% -i "%RAMSCRIPTDIR%\master_etf.sql"
