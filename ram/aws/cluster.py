9bca1a0e950254bae9550dbda4c7dd432fb04425
ryanoley <ryan@roundaboutam.com>
2016-11-29 11:46:34 -0500

    MOD: cleanup & pep8

--- ram/aws/cluster.py
+++ ram/aws/cluster.py
@@ -10,7 +10,7 @@ aws_data = os.path.join(os.getenv('DATA'), 'ram', 'aws')
 
 
 class ClusterController(object):
-    
+
     configured_regions = ['us-west-1a', 'us-west-2b']
     aws_config_base = os.path.join(os.getenv('GITHUB'), 'ram', 'ram', 'aws',
                                    'config')
@@ -20,9 +20,8 @@ class ClusterController(object):
         assert self.verify_sc_setup()
         self.islive = False
         # set strategy related vars
-        #iterable = strategy.get_terable()
-        #wrapper = strategy.get_wrapper()
-
+        # iterable = strategy.get_terable()
+        # wrapper = strategy.get_wrapper()
 
     def verify_sc_setup(self):
         # verify base config exists
@@ -35,9 +34,12 @@ class ClusterController(object):
         self.get_live_clusters()
         return True
 
-
     def set_config(self, n_cores, region=None, inst_type='m3.medium'):
-        # Set the instance type and n_nodes 
+        '''
+        Create a config for a particular cluster instance.  Fill in fields
+        for instance_type, region, n_nodes, and spot bid.  Write the
+        config out to /data/ram/aws/configs.
+        '''
         n_nodes = n_cores
         self._n_nodes = n_nodes
 
@@ -49,30 +51,32 @@ class ClusterController(object):
             bid = regionbid[1]
 
         # Read in base config
-        with open(self.aws_config_base, 'r') as file :
+        with open(self.aws_config_base, 'r') as file:
             configdata = file.read()
 
         # Configure and save as new
         inst_config = os.path.join(aws_data, 'configs',
                                    'config_{0}'.format(self._region))
         instdata = re.sub(r'CLUSTER_SIZE\s=\s\d+',
-                         'CLUSTER_SIZE = {0}'.format(n_nodes),
-                         configdata)
+                          'CLUSTER_SIZE = {0}'.format(n_nodes),
+                          configdata)
         instdata = re.sub(r'NODE_INSTANCE_TYPE.*?\n',
-                         'NODE_INSTANCE_TYPE = {0}\n'.format(inst_type),
-                         instdata)
+                          'NODE_INSTANCE_TYPE = {0}\n'.format(inst_type),
+                          instdata)
         instdata = re.sub(r'SPOT_BID.*?\n',
-                         'SPOT_BID = {0}\n'.format(bid),
-                         instdata)
+                          'SPOT_BID = {0}\n'.format(bid),
+                          instdata)
+
         with open(inst_config, 'w') as file:
             file.write(instdata)
-        
         self._config = inst_config
         return
 
-
     def get_region_bid(self, inst_type, region=None):
-        # Available regions
+        '''
+        Selects an available region based on configured_regions and any
+        live clusters. Prompts user for confirmation and returns a spot bid
+        '''
         clusters = self.get_live_clusters()
         inuse = [c['Zone'] for c in clusters.itervalues()]
         avail = list(set(self.configured_regions) - set(inuse))
@@ -96,11 +100,11 @@ class ClusterController(object):
 
         return (region, spot_info['max'])
 
-
     def launch_cluster(self):
+        '''
+        Launch cluster and wait for completion
+        '''
         assert os.path.exists(self._config)
-
-        # launch cluster and wait for completion
         cmd = 'starcluster -c {0} -r {1} start -c {2} {2}'.format(
             self._config, self._region[:-1], self._region)
         print 'Launching Cluster'
@@ -108,18 +112,18 @@ class ClusterController(object):
         print 'Cluster Launched Successfully'
         self.islive = True
 
-        # Setup parallel client 
+        # Setup parallel client
         ipy_json = re.findall(r"client\s=\sClient\('(.*?)'", stdout)[0]
         ipy_ssh = re.findall(r"sshkey='(.*?)'", stdout)[0]
         self._ipyconfig = (ipy_json, ipy_ssh)
         self.client = Client(ipy_json, sshkey=ipy_ssh)
         return
 
-
     def get_live_clusters(self):
-        # Return information on currently running clusters.  Return dictionary
-        # keyed on cluster name
-    
+        '''
+        Return information on currently running clusters. Outputs a
+        dictionary keyed on cluster name
+        '''
         all_regions = np.unique([x[:-1] for x in self.configured_regions])
         stdoutA = str()
         for region in all_regions:
@@ -137,7 +141,7 @@ class ClusterController(object):
         Zone = re.findall(r'Zone:\s(.*?)\r\n', stdoutA)
         Keypair = re.findall(r'Keypair:\s(.*?)\r\n', stdoutA)
         EbsVolumes = re.findall(r'EBS\svolumes:([\s\S]*?)Spot\srequests',
-                                stdoutA)  
+                                stdoutA)
         SpotRequests = re.findall(r'Spot\srequests:\s(.*?)\r\n', stdoutA)
         ClusterNodes = re.findall(r'Cluster\snodes:\r\n([\s\S]*?)' +
                                   'Total\snodes', stdoutA)
@@ -154,7 +158,7 @@ class ClusterController(object):
             clusters[name]['Zone'] = Zone[i]
             clusters[name]['Keypair'] = Keypair[i]
             clusters[name]['EbsVolumes'] = re.findall(r'\s+(.*)?\r\n',
-                                                        EbsVolumes[i])
+                                                      EbsVolumes[i])
             clusters[name]['SpotRequests'] = SpotRequests[i]
             clusters[name]['ClusterNodes'] = re.findall(r'\s+(.*)?\r\n',
                                                         ClusterNodes[i])
@@ -162,10 +166,10 @@ class ClusterController(object):
 
         return clusters
 
-
     def get_spot_history(self, inst_type, region):
-        # Get spot price information for instance & region
-
+        ''''
+        Return spot price information for instance & region
+        '''
         cmd = "starcluster -c {0} -r {1} spothistory -z {2} -c {3}".format(
             self.aws_config_base, region[:-1], region, inst_type)
         stdout = self._star_cmd(cmd)
@@ -173,26 +177,31 @@ class ClusterController(object):
         pLive = re.findall(r'Current\sprice:\s\$([\.\d]*?)\r\n', stdout)
         pMax = re.findall(r'Max\sprice:\s\$([\.\d]*?)\r\n', stdout)
         pAvg = re.findall(r'Average\sprice:\s\$([\.\d]*?)\r\n', stdout)
-        
-        return {'region':region, 'instance':inst_type, 'live':float(pLive[0]),
-                'max':float(pMax[0]), 'avg':float(pAvg[0])}
 
+        return {'region': region, 'instance': inst_type,
+                'live': float(pLive[0]), 'max': float(pMax[0]),
+                'avg': float(pAvg[0])}
 
     def run_parallel(self, function, iterable):
+        '''
+        Run function across a live cluster.  Iterable must be list-like
+        variable whose items can be passed to function.
+        '''
         assert hasattr(self, 'client')
         lv = self.client.load_balanced_view()
         results = lv.map(function, iterable)
         # Report progress
-        while results.ready() == False:
+        while not results.ready():
             ix = float(results.progress)
             print '{0}% Complete'.format(str(np.round(ix/len(iterable), 2) *
                                              100))
             time.sleep(10)
         return results
 
-
     def put_file(self, local_path, remote_path='/ramdata/temp'):
-        # Copy file to cluster, default is /ramdata/temp
+        '''
+        Copy file to cluster, default is /ramdata/temp
+        '''
         assert os.path.exists(local_path)
         assert self.islive
         cmd = 'starcluster -r {0} -c {1} put {2} {3} {4}'.format(
@@ -201,9 +210,10 @@ class ClusterController(object):
         stdout = self._star_cmd(cmd)
         return
 
-
     def get_file(self, local_path, remote_path):
-        # Copy file to cluster, will be placed under /ramdata
+        '''
+        Copy file to cluster, will be placed under /ramdata
+        '''
         assert os.path.exists(local_path)
         assert self.islive
         cmd = 'starcluster -r {0} -c {1} get {2} {3} {4}'.format(
@@ -212,9 +222,10 @@ class ClusterController(object):
         stdout = self._star_cmd(cmd)
         return
 
-
     def teardown(self):
-        # Shutdown cluster
+        '''
+        Shutdown cluster and cleanup instance variables
+        '''
         if self.islive is False:
             print 'No live cluster recognized. Please shutdown explicitly'
             return
@@ -227,9 +238,10 @@ class ClusterController(object):
         self._n_nodes = None
         return
 
-
     def restart_cluster(self):
-        # Reboot cluster 
+        '''
+        Reboot cluster
+        '''
         assert self.islive
         self.client.close()
         cmd = 'starcluster -r {0} -c {1} restart {2}'.format(
@@ -238,9 +250,10 @@ class ClusterController(object):
         self.client = Client(self._ipyconfig[0], sshkey=self._ipyconfig[1])
         return
 
-
     def shutdown_cluster(self, cluster_name):
-        # Shutdown a cluster
+        '''
+        Shutdown a cluster
+        '''
         cmd = 'starcluster -c {0} -r {1} terminate -f {2}'.format(
             self.aws_config_base, cluster_name[:-1], cluster_name)
         stdout = self._star_cmd(cmd, 'y')
@@ -252,10 +265,11 @@ class ClusterController(object):
         print 'Cluster shutdown complete'
         return
 
-
     def _star_cmd(self, cmd, stdin_txt=None):
-        # Run a command in subprocess, handle errors and return output from
-        # stdout
+        '''
+        Run a command in subprocess, handle errors and return output from
+        stdout
+        '''
         proc = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
         stdout, stderr = proc.communicate(stdin_txt)
         if proc.poll() != 0:
@@ -263,124 +277,50 @@ class ClusterController(object):
             raise
         return stdout
 
-
     def pull_git_branch(self, repo, branch):
-
+        '''
+        Pulls a branch from github in the specified repository. Reboots cluster
+        at end of function to ensure cluster is functioning.
+        '''
         assert self.islive
-
-        repo = 'earnings'
-        branch = 'awstest'
-
         nodes = self.get_live_clusters()[self._region]['ClusterNodes']
         assert len(nodes) == self._n_nodes
 
         for i in range(self._n_nodes):
-            if i == 0:
-                nodei = 'master'
-            else:
-                nodei = 'node00{0}'.format(i)
-
+            nodei = 'master' if i == 0 else 'node00{0}'.format(i)
             cmd = 'starcluster -r {0} -c {1} sn {2} {3} -u ubuntu'.format(
                 self._region[:-1], self._config, self._region, nodei)
+            # Open subprocess ssh to cluster node
             proc = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
-            nbsrO = NBSR(proc.stdout)
-            nbsrO.readuntil(timeout=5)
-
+            nbsr = NBSR(proc.stdout)
+            nbsr.readuntil(timeout=10)
+            # Change directory
             proc.stdin.write('cd ~/ramsrc/{} \n'.format(repo))
-            nbsrO.readuntil(re_error=r'No\ssuch\sfile\sor\sdirectory')
-
-            proc.stdin.write('git fetch \n')
-            nbsrO.readuntil(timeout=3, re_error=r'error')
-
-            proc.stdin.write('git checkout {} \n'.format(branch))
-            nbsrO.readuntil(re_error=r'error')
-
-            proc.stdin.write('git pull \n')
-            nbsrO.readuntil(timeout = 5, re_error=r'error')
-
+            nbsr.readuntil(re_error=r'No\ssuch\sfile\sor\sdirectory')
+            # /home is NFS shared accross cluster, pull only once on master
+            if nodei == 'master':
+                # Fetch new changes in repo
+                proc.stdin.write('git fetch \n')
+                nbsr.readuntil(timeout=7, re_error=r'error')
+                # Checkout desired branch
+                proc.stdin.write('git checkout {} \n'.format(branch))
+                nbsr.readuntil(re_error=r'error')
+                #  Pull latest changes
+                proc.stdin.write('git pull \n')
+                nbsr.readuntil(timeout=7, re_error=r'error')
+            # Update python libs on all nodes
             proc.stdin.write('sudo python setup.py install \n')
-            nbsrO.readuntil(timeout = 5,
-                            re_error=r'No\ssuch\sfile\sor\sdirectory')
-
+            nbsr.readuntil(timeout=7,
+                           re_error=r'No\ssuch\sfile\sor\sdirectory')
+            # Exit ssh session
             proc.stdin.write('exit \n')
-            nbsrO.readuntil()
-
+            nbsr.readuntil()
             print '/{1}/{2} updated on node: {0}'.format(nodei, repo, branch)
-
-
-
-
-
-
-
-
-# Testing functions
-iterable = range(30)
-def simple_iter(ix):
-    import socket
-    import time
-    host = socket.gethostname()
-    out_name = 'out_{0}${1}'.format(host, str(ix))
-    # pause to monitor progress
-    time.sleep(3)
-    return out_name
-
-iterable = range(5)
-def db_connect(dt):
-    import pypyodbc
-    connection = pypyodbc.connect('DSN=mssql;uid=ramuser;pwd=183madison')
-    cursor = connection.cursor()
-    status = connection.connected
-    connection.close()
-    return status
-
-import pandas as pd
-datelist = [x.date() for x in pd.date_range(pd.datetime.today(), periods=30)]
-def db_select(dt):
-    import datetime
-    import pypyodbc
-    import time
-    import pandas as pd
-    connection = pypyodbc.connect('DSN=mssql;uid=ramuser;pwd=183madison')
-    cursor = connection.cursor()
-    sqlcmd = "select * from ram.dbo.trading_dates where CalendarDate = '{}'".format(dt)
-    cursor.execute(sqlcmd)
-    out = pd.DataFrame(data=cursor.fetchall(), columns=['CalendarDate','Weekday','T0',
-                                                        'Tmd','Tm2','Tm1','T1','T2','T3'])
-    out.index = out.CalendarDate.values
-    connection.close()
-    time.sleep(3)
-    return out
-
-
-def db_write(dt):
-    import datetime
-    import pypyodbc
-    import pandas as pd
-    connection = pypyodbc.connect('DSN=mssql;uid=ramuser;pwd=183madison')
-    cursor = connection.cursor()
-    sqlcmd = "select * from ram.dbo.trading_dates where CalendarDate = '{}'".format(dt)
-    cursor.execute(sqlcmd)
-    out = pd.DataFrame(data=cursor.fetchall(),
-                       columns=['CalendarDate','Weekday','T0','Tmd','Tm2',
-                                'Tm1','T1','T2','T3'])
-    connection.close()
-    import socket
-    host = socket.gethostname()
-    out_name = '/ramdata/temp/out_{0}${1}.csv'.format(host, str(dt))
-    out.to_csv(out_name)
-    return host
-
-
-
-
+        print 'Rebooting Cluster'
+        self.restart_cluster()
 
 if __name__ == '__main__':
     cc = ClusterController('strat')
     cc.set_config(2)
-    #cc.launch_cluster()
+    # cc.launch_cluster()
     cc.teardown()
-
-
-
-
--- ram/aws/stream_reader.py
+++ ram/aws/stream_reader.py
@@ -1,17 +1,15 @@
-
 import re
 import time
 from threading import Thread
 from Queue import Queue, Empty
 
+
 class NonBlockingStreamReader:
 
     def __init__(self, stream):
         '''
-        stream: the stream to read from.
-                Usually a process' stdout or stderr.
+        stream: the stream to read from.  Usually a process' stdout or stderr.
         '''
-
         self._s = stream
         self._q = Queue()
 
@@ -19,7 +17,6 @@ class NonBlockingStreamReader:
             '''
             Collect lines from 'stream' and put them in 'queque'.
             '''
-
             while True:
                 line = stream.readline()
                 if line:
@@ -27,35 +24,31 @@ class NonBlockingStreamReader:
                 else:
                     raise UnexpectedEndOfStream
 
-        self._t = Thread(target = _populateQueue,
-                args = (self._s, self._q))
+        self._t = Thread(target=_populateQueue, args=(self._s, self._q))
         self._t.daemon = True
-        self._t.start() #start collecting lines from the stream
+        self._t.start()  # Start collecting lines from the stream
 
-    def readline(self, timeout = None):
+    def readline(self, timeout=None):
         try:
-            return self._q.get(block = timeout is not None,
-                    timeout = timeout)
+            return self._q.get(block=timeout is not None,
+                               timeout=timeout)
         except Empty:
             return None
 
-
-    def readuntil(self, timeout = 1, re_match=None, re_error=None):
+    def readuntil(self, timeout=1, re_match=None, re_error=None):
         '''
         Read from queue until re_match or re_error is found
         '''
         emptyline = 0
         time.sleep(timeout)
         self.stream_txt = self._stream_started()
-    
+
         while emptyline < 10:
             nextline = self.readline(.2)
-        
             if nextline is None:
                 emptyline += 1
                 continue
             self.stream_txt += nextline
-
             if re_match is not None:
                 match_s = re.search(re_match, nextline)
                 if match_s:
@@ -68,7 +61,6 @@ class NonBlockingStreamReader:
                     raise
         return
 
-
     def _stream_started(self):
         while True:
             nextline = self.readline(.2)
@@ -78,7 +70,5 @@ class NonBlockingStreamReader:
                 return nextline
 
 
-
-class UnexpectedEndOfStream(Exception): pass
-
-
+class UnexpectedEndOfStream(Exception):
+    pass
