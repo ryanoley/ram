6e5d7d5a261e0940f971aaa2383c5f29a214d02c
Mitchell Suter <mitchellasuter@gmail.com>
2016-12-06 15:30:00 -0500

    MOD: sector table adjusted

--- ram/data/qad/data_compustat_sector.sql
+++ ram/data/qad/data_compustat_sector.sql
@@ -1,26 +1,40 @@
 
 -- Compustat historical Sector data
-
 if object_id('ram.dbo.ram_sector', 'U') is not null 
 	drop table ram.dbo.ram_sector
 
 
 create table	ram.dbo.ram_sector (
-				SecCode int,
-				IdcCode int,
+				IsrCode int,
 				GVKey int,
 				GSECTOR int,
 				GGROUP int,
 				StartDate smalldatetime,
 				EndDate smalldatetime
-				primary key (SecCode, IdcCode, StartDate, EndDate)
+				primary key (IsrCode, GVKey, StartDate, EndDate)
 )
 
 
-; with master_ids as (
+; with mdata as (
 
-select distinct		IdcCode, IsrCode, SecCode 
-from				ram.dbo.ram_master_equities
+select distinct		C.GVKey as GVKey, 
+					U.SecCode,
+					U.IsrCode,
+					U.Date_
+from				ram.dbo.ram_master_equities2 U
+
+	join			qai.dbo.SecMapX M
+	on				U.SecCode = M.SecCode
+	and				M.VenType = 4
+	and				M.Exchange = 1
+	and				U.Date_ between M.StartDate and M.EndDate
+
+	join			qai.dbo.CSVSecurity C
+	on				M.VenCode = C.SecIntCode
+	and				C.TPCI = '0'
+	and				C.EXCNTRY = 'USA'
+
+where				U.Date_ < C.DLDTEI
 
 )
 
@@ -33,73 +47,15 @@ select GVKey, INDFROM, coalesce(INDTHRU, getdate()) as INDTHRU, GSECTOR, GGROUP 
 )
 
 
-, data1 as (
--- Naive join to get SecIntCodes/GVKey
-select			ID.*,
-				M1.VenCode as SecIntCode,
-				M2.GVKey as GVKey
-
-from			master_ids ID
-
-left join		qai.dbo.SecMapX M1
-	on			M1.SecCode = ID.SecCode
-	and			M1.VenType = 4			-- Compustat
-	and			M1.Exchange = 1
-
-left join		qai.dbo.CSVSecurity M2
-	on			M1.VenCode = M2.SecIntCode
-
-)
-
-
-, data2 as (
--- Multiple issues don't map well to SecCodes, therefore
--- go through IsrCode
-select			IsrCode,
-				max(GVKey) as GVKey
-from			data1
-group by		IsrCode
-
-)
-
-
-, data3 as (
-select			D1.IdcCode,
-				D1.SecCode,
-				D2.GVKey,
-				C.GSECTOR,
-				C.GGROUP,
-				C.INDFROM as StartDate,
-				C.INDTHRU as EndDate
-from			data1 D1
-join			data2 D2
-	on			D1.IsrCode = D2.IsrCode
-join			sectors C
-	on			D2.GVKey = C.GVKey
-)
-
-
-, data4 as (
--- Historical table only goes to 1999. Assume that sector is same as first
--- observation in table
-select			SecCode,
-				IdcCode,
-				GVKey,
-				GSECTOR,
-				GGROUP,
-				case 
-					when StartDate = (
-						select min(b.StartDate) 
-						from data3 b 
-						where b.IdcCode = A.IdcCode 
-						and b.GVKey = A.GVKey)
-					then '1993-01-01'
-					else StartDate
-				end as StartDate,
-				EndDate
-from			data3 A
-)
-
-
 insert into		ram.dbo.ram_sector
-select distinct * from	data4
+select					M.IsrCode,
+						S.GVKey,
+						S.GSECTOR,
+						S.GGROUP,
+						min(M.Date_) as StartDate,
+						max(M.Date_) as EndDate
+from					mdata M
+join					sectors S
+	on					M.GVKey = S.GVKey
+	and					M.Date_ between S.INDFROM and S.INDTHRU
+group by				M.IsrCode, S.GVKey, S.GSECTOR, S.GGROUP
