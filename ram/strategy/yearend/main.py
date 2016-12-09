b0c60b3a9306f7d2400326c95a94ef1edcd4dcfe
ryanoley <ryan@roundaboutam.com>
2016-12-09 14:39:15 -0500

    MOD: YearEnd main cleanup

--- ram/strategy/yearend/main.py
+++ ram/strategy/yearend/main.py
@@ -10,93 +10,148 @@ class YearEnd(Strategy):
 
     COST = 0.0015
 
-    def get_iter_index(self, hold_per=5):
-        return self._get_date_iterator()
-    
+    def __init__(self, hold_per=5, univ_size=1000, port_n=50, ind_n=np.inf,
+                 output_dir=None):
+        super( YearEnd, self ).__init__()
+        self.date_iter = self._get_date_iterator(hold_per)
+        self.hold_per = hold_per
+        self.univ_size = univ_size
+        self.port_n = port_n
+        self.ind_n = ind_n
+        self.allocs = pd.DataFrame([])
+
+    def get_iter_index(self):
+        return self.date_iter[5:]
+
     def run_index(self, index):
-        pass
+        # Params
+        train_pers = 5
+        ix = self.date_iter.index(index)
+        
+        # Generate training and test data frames
+        train = self.get_univ_iter(self.date_iter[ix - train_pers : ix])
+        test = self.get_univ_ix(index)
+        train = train.dropna()
+        test = test.dropna()
+        
+        # Get signals for Test
+        features = list(set(train.columns) - set(['SecCode', 'Date', 'Vwap',
+                                                  'LEAD5_Vwap', 'RClose',
+                                                  'MarketCap', 'AvgDolVol',
+                                                  'GSECTOR', 'Ret', 'RetH']))
+        signals = self.generate_signals(train[features], train.RetH,
+                                        test[features])
+
+        # Set portfolios based on Ind and Rank
+        ranks = pd.DataFrame(data={'SecCode':test.SecCode,
+                                   'Ind':test.GSECTOR,
+                                   'Rank':np.argsort(np.argsort(signals))})
+        ranks.sort_values('Rank', inplace=True)
+        port_ix = self._get_portfolio_ix(ranks.Rank,
+                                         ranks.Ind,
+                                         n = self.port_n,
+                                         max_i = self.ind_n)
+
+        # Get daily returns
+        codes = list(test.loc[port_ix, 'SecCode'])
+        rets = self._get_dailys_rets(codes, index[1], index[2])
+        grp = rets.groupby('Date')
+        port_rets = pd.DataFrame(grp.RetH.mean())
+        
+        # Save allocs
+        self.allocs = self.allocs.append(rets)
+        print str(index[2])
+        return port_rets
 
     def _get_date_iterator(self, hold_per=5):
         """
-        Iterator with revlevant dates (eval_dt, entry_dt, exit_dt)
+        Produce iterable with revlevant dates (eval_dt, entry_dt, exit_dt)
         for each event (year end)
         """
         db_dates = self.datahandler.get_all_dates()
 
-        fdoy = pd.date_range('1996-01-01', '2016-01-01', freq='AS')
+        fdoy = pd.date_range('2000-01-01', '2016-01-01', freq='AS')
         fdoy = np.array([x.to_pydatetime() for x in fdoy])
-
         fbdoy =  np.array([db_dates[db_dates > x].min() for x in fdoy])
         lbdoy =  np.array([db_dates[db_dates < x].max() for x in fdoy])
 
         lbdix = np.array([np.where(db_dates == x)[0][0] for x in lbdoy])
         entryix = lbdix - hold_per
         evalix = entryix - 1
-        
-        # Get train start, test start, and final quarter date
-        iterable = zip(db_dates[evalix], db_dates[entryix],
-                       db_dates[lbdix])
+
+        iterable = zip(db_dates[evalix], db_dates[entryix], db_dates[lbdix])
         return iterable
 
-
-    def _create_ix_features(self, eval_date, entry_date, exit_date,
-                            hold_per=5, univ_size = 1000):
+    def get_univ_iter(self, dt_iter):
         '''
-        Creates features for a single index of eval, entry and exit dates.
-        Also takes parameters for the holding_period length and universe size.
+        Loop through dt_iter and append results together
         '''
-        filter_args = {'filter': 'AvgDolVol',
-                       'where': 'MarketCap >= 100 and Close_ >= 10',
-                       'univ_size': univ_size}
+        univ = pd.DataFrame([])
+        for index in dt_iter:
+            ix_df = self.get_univ_ix(index)
+            univ = univ.append(ix_df)
+        return univ.reset_index(drop=True)
 
-        features = ['Vwap', 'LEAD{}_Vwap'.format(hold_per),
-                    'LAG1_RClose', 'LAG1_MarketCap', 'LAG1_AvgDolVol',
+    def get_univ_ix(self, index):
+        '''
+        Create features for a single index of eval, entry and exit dates.
+        Pass hold_per and univ_size here.
+        '''
+        # Set dates and args for sql datahandler
+        (eval_date, entry_date, exit_date) = index
+
+        ExitCol = 'LEAD{}_Vwap'.format(self.hold_per)
+        features = ['Vwap', ExitCol, 'GSECTOR', 'LAG1_RClose', 'SI', 
+                    'LAG1_MarketCap', 'LAG1_AvgDolVol',
                     'LAG1_PRMA30_Close', 'LAG1_PRMA60_Close',
                     'LAG1_PRMA120_Close', 'LAG1_PRMA250_Close',
                     'LAG1_BOLL30_Close', 'LAG1_BOLL60_Close',
                     'LAG1_BOLL120_Close', 'LAG1_BOLL250_Close',
                     'LAG1_RSI30_Close', 'LAG1_RSI60_Close',
                     'LAG1_RSI120_Close', 'LAG1_RSI250_Close',
-                    'LAG1_DISCOUNT30_Close','LAG1_DISCOUNT60_Close',
+                    'LAG1_DISCOUNT30_Close', 'LAG1_DISCOUNT60_Close',
                     'LAG1_DISCOUNT120_Close', 'LAG1_DISCOUNT250_Close',
                     'LAG1_VOL30_Close', 'LAG1_VOL60_Close',
                     'LAG1_VOL120_Close', 'LAG1_VOL250_Close',
                     'LAG1_MFI30_Close', 'LAG1_MFI60_Close',
-                    'LAG1_MFI120_Close', 'LAG1_MFI250_Close', 'GSECTOR']
+                    'LAG1_MFI120_Close', 'LAG1_MFI250_Close']
+
+        # Pull data and filter to date of interest
+        filter_args = {'filter': 'AvgDolVol',
+                       'where': 'MarketCap >= 100 and Close_ >= 10',
+                       'univ_size': self.univ_size}
 
         df = self.datahandler.get_filtered_univ_data(
             features=features,
             start_date=entry_date,
-            end_date=exit_date,
+            end_date=entry_date,
             filter_date=eval_date,
             filter_args=filter_args)
-        df = df.loc[df.Date == entry_date]
 
-        # Market Variables
-        spy_features = ['Vwap', 'LEAD{}_Vwap'.format(hold_per),
+        # Hedge these vars with SPY
+        spy_features = ['Vwap', ExitCol,
                         'LAG1_PRMA30_Close', 'LAG1_PRMA60_Close',
                         'LAG1_PRMA120_Close', 'LAG1_PRMA250_Close']
         spy = self.datahandler.get_etf_data(
             ['SPY'],
             features=spy_features,
             start_date=entry_date,
-            end_date=exit_date)
-        spy = spy.loc[spy.Date == entry_date]
-
-        #  Add Returns
-        df['Ret'] = (df['LEAD{}_Vwap'.format(hold_per)] / df.Vwap) - 1
-        df['RetH'] = ((df['LEAD{}_Vwap'.format(hold_per)] / df.Vwap) -
-            float(spy['LEAD{}_Vwap'.format(hold_per)] / spy.Vwap))
+            end_date=entry_date)
 
         # Hedge Additional measures
         for col in spy_features[2:]:
             df[col] -= float(spy[col])
-        
-        # Rename columns
-        df.rename(columns={x:x.split('_')[1] for x in features[2:-1]},
-                  inplace=True)
 
-        # Industry performance for selected variables
+        #  Add Returns
+        df['Ret'] = (df[ExitCol] / df.Vwap) - 1
+        df['RetH'] = (df[ExitCol] / df.Vwap) - float(spy[ExitCol] / spy.Vwap)
+
+        # Rename columns
+        skip_cols = ['Vwap', ExitCol, 'GSECTOR', 'SI']
+        df.rename(columns={x:x.split('_')[1] for x in features if
+                           x not in skip_cols}, inplace=True)
+
+        # Columns to average performance over Industry
         avg_cols = ['PRMA30', 'PRMA60', 'PRMA120', 'PRMA250',
                     'DISCOUNT30', 'DISCOUNT60', 'DISCOUNT120', 'DISCOUNT250',
                     'RSI30', 'RSI60', 'RSI120', 'RSI250',
@@ -104,10 +159,13 @@ class YearEnd(Strategy):
         ind_data = self._industry_avg(df.GSECTOR, df[avg_cols])
         df = df.merge(ind_data, left_index=True, right_index=True)
         
-        # Rank 
+        # Columns to add Rank Vars 
         rank_cols = ['DISCOUNT30', 'DISCOUNT60', 'DISCOUNT120', 'DISCOUNT250']
         rank_data = self._rank_cols(df[rank_cols])
         df = df.merge(rank_data, left_index=True, right_index=True)
+
+        # Temp fix for na SI values
+        df.SI = df.SI.fillna(0)
 
         return df.reset_index(drop=True)
 
@@ -138,32 +196,115 @@ class YearEnd(Strategy):
             ranks = np.argsort(np.argsort(base_data[col]))
             rank_data.loc[:, 'Rank_{}'.format(col)] = ranks
         return rank_data
-
-    def _create_univ_df(self, dt_iter, univ_size, hold_per):
+  
+    def _get_portfolio_ix(self, scores, industries, n, max_i=np.inf):
         '''
-        Loop through dt_iter and append results together.
+        Select a group of n scores limiting the total in any industry
+        by the max_i parameter. Returns the index of selection.
         '''
-        univ = pd.DataFrame([])
+        ind_counts = {x:0 for x in industries.unique()}
+        sel_ix = []
+        
+        while (len(sel_ix) < n) and (len(scores) > 0):
+            ix = scores.index[-1]
+            ind = industries.pop(ix)
+            score = scores.pop(ix)
+            if ind_counts[ind] < max_i:
+                sel_ix.append(ix)
+                ind_counts[ind] += 1
+        return sel_ix
 
-        for (dt_eval, dt_entry, dt_exit) in dt_iter:
-            ix_df = self._create_ix_features(dt_eval, dt_entry, dt_exit,
-                                             hold_per = hold_per,
-                                             univ_size = univ_size)
-            univ = univ.append(ix_df)
-            print str(dt_exit)
-        return univ.reset_index(drop=True)
+    def _get_dailys_rets(self, codes, start, end):
+        '''
+        Get the daily returns of a list of SecCodes between start and
+        end dates.
+        '''
+        # SPY Returns
+        spy = self.datahandler.get_etf_data(
+            ['SPY'],
+            features=['Vwap', 'Close'],
+            start_date=start,
+            end_date=end)
+        spy['PriorClose'] = spy.Close.shift(1)
+        spy['R1'] = spy.Close / spy.Vwap
+        spy['R2'] = spy.Close / spy.PriorClose
+        spy['R3'] = spy.Vwap / spy.PriorClose
+        spy['MktRet'] = spy.R2
+        spy.loc[spy.Date == start, 'MktRet']  = spy.R1
+        spy.loc[spy.Date == end, 'MktRet']  = spy.R3
+        
+        # Equity Returns
+        returns = self.datahandler.get_id_data(
+            ids=codes,
+            features=['Vwap', 'Close'],
+            start_date=start,
+            end_date=end)
+        returns.sort_values(['ID','Date'], inplace=True)
+
+        # Could do this all at once, concern is missing data        
+        for i in returns.ID.unique():
+            tmp = returns.copy()
+            tmp['PriorClose'] = tmp.Close.shift(1)
+            tmp['R1'] = tmp.Close / tmp.Vwap
+            tmp['R2'] = tmp.Close / tmp.PriorClose
+            tmp['R3'] = tmp.Vwap / tmp.PriorClose
+            tmp['Ret'] = tmp.R2
+            tmp.loc[tmp.Date == start, 'Ret']  = tmp.R1 - (self.COST / 2)
+            tmp.loc[tmp.Date == end, 'Ret']  = tmp.R3 - (self.COST / 2)
+            returns.loc[tmp.index, 'Ret'] = tmp.Ret
+        
+        returns = returns.merge(spy[['Date','MktRet']], how='left')
+        returns['RetH'] = returns.Ret - returns.MktRet
+        returns.Ret -= 1
+        return returns
+
+    def generate_signals(self, train, resp, test):
+        '''
+        Produce a  measure which can be sorted and used as selection
+        criteria for portfolio formation.  Higher vals are selected first.
+        '''
+        from sklearn.linear_model import LinearRegression
+        from sklearn.ensemble import RandomForestRegressor
+
+        model = LinearRegression()
+        model.fit(train, resp)
+        lr_preds = model.predict(test)
+        rfmodel = RandomForestRegressor(n_estimators = 100,
+                                        min_samples_split=250,
+                                        min_samples_leaf=100,
+                                        random_state=123)
+        rfmodel.fit(train, resp)
+        rf_preds = rfmodel.predict(test)
+
+        return rf_preds
 
 
 if __name__ == '__main__':
-        
-    # PARAMS
-    hold_per = 5
-    univ_size = 1500
-
-    strategy = YearEnd()
-    dt_iter = strategy.get_iter_index(hold_per)
-    univ = strategy._create_univ_df(dt_iter, univ_size, hold_per)
-
+    from ram.strategy.yearend.main import YearEnd
+    # whole file
+    univ = ye.get_univ_iter(ye.date_iter)
     univ = pd.read_csv('C:/temp/yearend.csv')
 
+    ye = YearEnd(hold_per = 5, univ_size = 100, port_n=10, ind_n=10)
+    ye.start()
+    
+    #Single index    
+    dt_iter = ye.get_iter_index()
+    index = dt_iter[7]
+    rets = ye.run_index(index)
+    
+    # Cloud
+    def parallel_fcn(index):
+        from ram.strategy.yearend.main import YearEnd
+        child = YearEnd(hold_per = 5, univ_size = 100, port_n=10, ind_n=10)
+        result = child.run_index(index)
+        return result
+
+    dt_iter = ye.get_iter_index()
+    from ram.aws.cluster import ClusterController
+    cc = ClusterController('strat')
+    cc.set_config(3)
+    cc.launch_cluster()
+    cc.pull_git_branch('ram','master')
+    results = cc.run_parallel(parallel_fcn, dt_iter[:5])
 
