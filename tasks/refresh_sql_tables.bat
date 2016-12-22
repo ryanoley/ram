9def92bbb40d88afecc8df1b8a130ed0e73a469a
RAMServer <analysts@roundaboutam.com>
2016-12-22 15:43:58 -0500

    BUG: typo in batch file for etf sql script

--- tasks/refresh_sql_tables.bat
+++ tasks/refresh_sql_tables.bat
@@ -4,6 +4,6 @@ set RAMSCRIPTDIR=%GITHUB%\ram\ram\data\qad
 set SQLCMDAUTH="%SQLCMDPATH%\sqlcmd" -S 192.168.2.8 -d ram -U ramuser -P 183madison
 
 %SQLCMDAUTH% -i "%RAMSCRIPTDIR%\master_equities.sql"
-%SQLCMDAUTH% -i "%RAMSCRIPTDIR%\master_etfs.sql"
+%SQLCMDAUTH% -i "%RAMSCRIPTDIR%\master_etf.sql"
 %SQLCMDAUTH% -i "%RAMSCRIPTDIR%\data_compustat_sector.sql"
 %SQLCMDAUTH% -i "%RAMSCRIPTDIR%\table_monitor.sql"
