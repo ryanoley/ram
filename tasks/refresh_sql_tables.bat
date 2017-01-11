751e73ff6abf93ac1ae0a1211ff71eaa2b4ca1b7
ryanoley <ryan@roundaboutam.com>
2017-01-11 13:36:21 -0500

    MOD: add status check into refresh tables bat

--- tasks/refresh_sql_tables.bat
+++ tasks/refresh_sql_tables.bat
@@ -1,5 +1,6 @@
 
 set RAMSCRIPTDIR=%GITHUB%\ram\ram\data\qad
+set PYSCRIPTDIR=%GITHUB%\ram\tasks
 
 set SQLCMDAUTH="%SQLCMDPATH%\sqlcmd" -S 192.168.2.8 -d ram -U ramuser -P 183madison
 
@@ -7,3 +8,8 @@ set SQLCMDAUTH="%SQLCMDPATH%\sqlcmd" -S 192.168.2.8 -d ram -U ramuser -P 183madi
 %SQLCMDAUTH% -i "%RAMSCRIPTDIR%\master_etf.sql"
 %SQLCMDAUTH% -i "%RAMSCRIPTDIR%\data_compustat_sector.sql"
 %SQLCMDAUTH% -i "%RAMSCRIPTDIR%\table_monitor.sql"
+
+
+:: Check status of QAD and RAM tables and send alerts if necessary
+python %PYSCRIPTDIR%\db_monitor.py -update_qad_status -write_log
+
