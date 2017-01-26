ff3ccf22c34d0bb90208e1f71c69a2be527631e2
Mitchell Suter <mitchellasuter@gmail.com>
2017-01-26 17:35:54 -0500

    MOD: updated task batch file for gvkes

--- tasks/refresh_sql_tables.bat
+++ tasks/refresh_sql_tables.bat
@@ -4,6 +4,7 @@ set PYSTATUSDIR=%GITHUB%\ram\tasks
 
 set SQLCMDAUTH="%SQLCMDPATH%\sqlcmd" -S 192.168.2.8 -d ram -U ramuser -P 183madison
 
+%SQLCMDAUTH% -i "%RAMSCRIPTDIR%\gvkey_map.sql"
 %SQLCMDAUTH% -i "%RAMSCRIPTDIR%\master_equities.sql"
 %SQLCMDAUTH% -i "%RAMSCRIPTDIR%\master_etf.sql"
 %SQLCMDAUTH% -i "%RAMSCRIPTDIR%\data_compustat_sector.sql"
