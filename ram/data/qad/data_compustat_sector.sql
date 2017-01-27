f9a5bc6b170d8b589d7ccf63312fd87390850cb0
Mitchell Suter <mitchellasuter@gmail.com>
2017-01-27 11:18:02 -0500

    MOD: new compustat sector table

--- ram/data/qad/data_compustat_sector.sql
+++ ram/data/qad/data_compustat_sector.sql
@@ -14,73 +14,19 @@ create table	ram.dbo.ram_sector (
 				primary key (SecCode, GVKey, StartDate, EndDate)
 )
 
-
-; with ids1 as (
-select distinct IsrCode, SecCode from ram.dbo.ram_master_equities
-)
-
-
-, gvkey1 as (
-select		I.IsrCode,
-			I.SecCode,
-			C.GVKey
-from		ids1 I
-join		SecMapX M
-	on		I.SecCode = M.SecCode
-	and		M.VenType = 4
-	and		M.Exchange = 1
-	and		M.[Rank] = 1
-join		CSVSecurity C
-	on		M.VenCode = C.SecIntCode
-	and		C.EXCNTRY = 'USA'
-	and		C.TPCI = '0'
-)
-
-
-, ids2 as (
-
-select distinct IsrCode, SecCode from ids1
-except
-select distinct IsrCode, SecCode from gvkey1
-
-)
-
-
-, gvkey2 as (
-
-select			I.IsrCode,
-				I.SecCode,
-				G.GVKEY
-from			ids2 I
-join			gvkey1 G
-	on			I.IsrCode = G.IsrCode
-
-)
-
-
-, gvkeys as (
-select * from gvkey1
-union
-select * from gvkey2
-)
-
-
-, sectors as (
-
+; with sectors as (
 select GVKey, INDFROM, coalesce(INDTHRU, getdate()) as INDTHRU, GSECTOR, GGROUP from qai.dbo.CSCoHGIC
 union
 select GVKey, INDFROM, coalesce(INDTHRU, getdate()) as INDTHRU, GSECTOR, GGROUP from qai.dbo.CSICoHGIC
-
 )
 
-
-insert into				ram.dbo.ram_sector
-select distinct			M.SecCode,
-						S.GVKey,
-						S.GSECTOR,
-						S.GGROUP,
-						S.INDFROM as StartDate,
-						S.INDTHRU as EndDate
-from					gvkeys M
-join					sectors S
-	on					M.GVKey = S.GVKey
+insert into		ram.dbo.ram_sector
+select			M.SecCode,
+				M.GVKey,
+				S.GSECTOR,
+				S.GGROUP,
+				S.INDFROM as StartDate,
+				S.INDTHRU as EndDate
+from			sectors S
+	join		(select distinct SecCode, GVKey from ram.dbo.ram_master_equities) M
+	on			S.GVKey = M.GVKey
