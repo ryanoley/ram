a0320195c5ac15295f6bff069f99cf7792b1e704
mitchellsuter <mitchellasuter@gmail.com>
2017-02-06 18:43:43 -0500

    MOD: iterates over different params

--- ram/strategy/statarb/main.py
+++ ram/strategy/statarb/main.py
@@ -1,3 +1,4 @@
+import json
 import itertools
 import numpy as np
 import pandas as pd
@@ -28,15 +29,43 @@ class StatArbStrategy(Strategy):
         data = self._get_data(t_start, cut_date, t_end,
                               univ_size=self.univ_size)
 
-        args1 = {'z_window': 30, 'max_pairs': 1000}
-        scores, pair_info = self.pairselector.get_best_pairs(
-            data, cut_date, **args1)
+        args1 = make_arg_iter({
+            'z_window': [30, 40, 50, 60, 100, 140],
+            'max_pairs': [1000, 1500]
+        })
 
-        args2 = {'n_pairs': 100, 'max_pos_prop': 0.05}
-        results = self.constructor.get_daily_pl(
-            scores, data, pair_info, **args2)
+        args2 = make_arg_iter({
+            'n_pairs': [80, 120, 180],
+            'max_pos_prop': [0.05, 0.10],
+            'pos_perc_deviation': [0.3, 0.5, 0.7],
+        })
 
-        return results
+        ind = 0
+        output_results = pd.DataFrame()
+        output_params = {}
+
+        for a1 in args1:
+            scores, pair_info = self.pairselector.get_best_pairs(
+                data, cut_date, **a1)
+
+            for a2 in args2:
+                results = self.constructor.get_daily_pl(
+                    scores, data, pair_info, **a2)
+                results.columns = [ind]
+                output_results = output_results.join(results, how='outer')
+                ind += 1
+
+                temp_params = {}
+                temp_params.update(a1)
+                temp_params.update(a2)
+
+                output_params[ind] = temp_params
+
+        # write
+        with open('C:\Users\Mitchell\Desktop\stat_arb_params.json', 'w') as outfile:
+            json.dump(output_params, outfile)
+
+        return output_results
 
     def _get_date_iterator(self):
         """
@@ -84,6 +113,11 @@ class StatArbStrategy(Strategy):
         data.SecCode = data.SecCode.astype(str)
 
         return data
+
+
+def make_arg_iter(variants):
+    return [{x: y for x, y in zip(variants.keys(), vals)}
+        for vals in itertools.product(*variants.values())]
 
 
 if __name__ == '__main__':
