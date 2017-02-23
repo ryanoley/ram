c3fa25f97eff9442b534a0147dc33a7c378bc42a
Mitchell Suter <mitchellasuter@gmail.com>
2017-02-23 13:57:13 -0500

    MOD: update to examples for combo search

--- examples/combination_search.py
+++ examples/combination_search.py
@@ -11,35 +11,11 @@ if __name__ == '__main__':
     parser.add_argument(
         '-r', '--restart', action='store_true',
         help='Restart simulation')
-    parser.add_argument(
-        '-n', '--search_name',
-        help='Name of directory')
     args = parser.parse_args()
 
-    BASE = 'C:/Users/Mitchell/Desktop/StatArbStrategy'
-    dpath = os.path.join(BASE, args.search_name)
-    comb = CombinationSearch(dpath)
+    comb = CombinationSearch('StatArbStrategy')
 
     if args.restart:
         comb.restart()
-
     else:
-        dpath = os.path.join(BASE, 'v1')
-        df = CombinationSearch._read_csv(os.path.join(dpath, 'results.csv'))
-        comb.add_data(df, 'commit_1')
-
-        dpath = os.path.join(BASE, 'v2')
-        df = CombinationSearch._read_csv(os.path.join(dpath, 'results.csv'))
-        comb.add_data(df, 'commit_2')
-
-        dpath = os.path.join(BASE, 'v3')
-        df = CombinationSearch._read_csv(os.path.join(dpath, 'results.csv'))
-        comb.add_data(df, 'commit_3')
-
-        dpath = os.path.join(BASE, 'v4')
-        df = CombinationSearch._read_csv(os.path.join(dpath, 'results.csv'))
-        comb.add_data(df, 'commit_4')
-
-        comb.set_training_params(freq='w', n_periods=104,
-                                 n_ports_per_combo=5, n_best_combos=10)
-        comb.start()
+        comb.start('run_0001')
