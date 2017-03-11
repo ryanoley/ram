db5224806cb371b17bcc7716a9014407299bba06
Mitchell Suter <mitchellasuter@gmail.com>
2017-03-11 14:31:28 -0500

    MOD: reorg of sql pull tables

--- tasks/etl_ram_sql_tables.bat
+++ tasks/etl_ram_sql_tables.bat
@@ -4,12 +4,14 @@ set SQLCMD="%SQLCMDPATH%\sqlcmd" -S 192.168.2.8 -d ram -U ramuser -P 183madison
 set SQLDIR=%GITHUB%\ram\data\ram_sql_tables
 set TASKDIR=%GITHUB%\ram\tasks
 
+:: Master ID Tables
+%SQLCMD% -i %SQLDIR%\ram_master_ids.sql
+%SQLCMD% -i %SQLDIR%\ram_master_ids_etf.sql
+
 :: GVKEY Mapping
 call %SQLDIR%\gvkey_idc_mapping\get_map_data.bat
 
 :: RAM Tables
-%SQLCMD% -i %SQLDIR%\ram_master_ids.sql
-%SQLCMD% -i %SQLDIR%\ram_master_ids_etf.sql
 %SQLCMD% -i %SQLDIR%\ram_compustat_sector.sql
 %SQLCMD% -i %SQLDIR%\ram_equity_report_dates.sql
 %SQLCMD% -v tabletype=1 -i %SQLDIR%\ram_equity_pricing.sql
