f279364d93c5a8044cfd91a00ab5e43f9357c82e
mitchellsuter <mitchellasuter@gmail.com>
2017-03-22 17:08:36 -0400

    MOD: data prep for birds

--- ram/strategy/birds/data_prep.py
+++ ram/strategy/birds/data_prep.py
@@ -2,15 +2,24 @@
 from ram.data.constructor import DataConstructor
 
 
-features = ['AvgDolVol', 'MarketCap']
+features = ['AvgDolVol', 'MarketCap', 'AdjClose',
+            'LEAD1_AdjClose', 'LEAD2_AdjClose']
 
 features += ['PRMA5_AvgDolVol', 'PRMA10_AvgDolVol', 'PRMA20_AvgDolVol']
 features += ['PRMA5_AdjClose', 'PRMA10_AdjClose', 'PRMA20_AdjClose']
+features += ['VOL5_AdjClose', 'VOL10_AdjClose', 'VOL20_AdjClose']
+features += ['BOLL5_AdjClose', 'BOLL10_AdjClose', 'BOLL20_AdjClose']
+features += ['DISCOUNT63_AdjClose', 'DISCOUNT126_AdjClose',
+             'DISCOUNT252_AdjClose']
+features += ['RSI5_AdjClose', 'RSI10_AdjClose', 'RSI20_AdjClose']
+features += ['MFI5_AdjClose', 'MFI10_AdjClose', 'MFI20_AdjClose']
+
+# Fundamentals that only really change with an earnings announcement
+features += ['ACCTPRICESALES', 'ACCTEPSGROWTH', 'EARNINGSRETURN']
+features += ['SI']
 
 dc = DataConstructor('BirdsStrategy')
-dc.register_dates_parameters('Q', 0, 2016)
+dc.register_dates_parameters('Q', 0, 2000)
 dc.register_features(features)
-dc.register_universe_size(10)
+dc.register_universe_size(1000)
 dc.run()
-
-
