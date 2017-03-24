a0bfa3dce466b4b297ba59a7cb89b8597d2a1785
mitchellsuter <mitchellasuter@gmail.com>
2017-03-24 08:58:01 -0400

    MOD: routine

--- ram/config.py
+++ ram/config.py
@@ -3,3 +3,5 @@ import os
 BASE_DIR = os.path.join(os.getenv('DATA'), 'ram')
 
 PREPPED_DATA_DIR = os.path.join(BASE_DIR, 'prepped_data')
+
+SIMULATION_OUTPUT_DIR = os.path.join(BASE_DIR, 'simulations')
--- ram/strategy/base.py
+++ ram/strategy/base.py
@@ -1,7 +1,5 @@
 import os
 import json
-import shutil
-import subprocess
 import pandas as pd
 import datetime as dt
 
