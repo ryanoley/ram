b9f6965d1e049b7288239905cb4e617dd05ba69e
ryanoley <ryan@roundaboutam.com>
2017-04-17 17:39:25 -0400

    MOD: gap strategy work

--- ram/strategy/gap/main.py
+++ ram/strategy/gap/main.py
@@ -4,94 +4,142 @@ import pandas as pd
 import datetime as dt
 import itertools
 
-from gearbox import read_csv
+from gearbox import read_csv, convert_date_array
 from ram.strategy.base import Strategy
-from ram.data.data_handler_sql import DataHandlerSQL
+
 
 COST = 0.0015
+AUM = 98e6
+
 
 class GapStrategy(Strategy):
     
     def get_column_parameters(self):
         z_args = make_arg_iter({'z': [.5, .75, 1., 1.25, 1.5]})
+        mktZArgs = make_arg_iter({'mktZ': [.5, .75, 1., 1.25, 1.5, 5.]})
         output = {}
-        for i, x in enumerate(list(z_args)):
-            output[i] = x
+        for i, (x, y) in enumerate(list(itertools.product(z_args, mktZArgs))):
+            z = {}
+            z.update(x)
+            z.update(y)
+            output[i] = z
         return output
-
+    
     def run_index(self, index):
-        
+        max_gross = 10e6
+        base_pos = .005
+
+        args1 = make_arg_iter({'z': [.5, .75, 1., 1.25, 1.5]})
+        mktZ = [.5, .75, 1., 1.25, 1.5, 5.]
+
         data = self.read_data_from_index(index)
         data = self.create_features(data)
-
-        iter_params = make_arg_iter({'z': [.5, .75, 1., 1.25, 1.5]})
+        mktData = pd.read_csv("C:/temp/market_data.csv")
+        mktData.Date = convert_date_array(mktData.Date)
 
         ind = 0
         output_results = pd.DataFrame()
-        for params in iter_params:
-            results = self.get_trades(data, **params)
-            results.columns = [ind]
-            output_results = output_results.join(results, how='outer')
-            ind += 1
+        output_stats = {}
+
+        for arg in args1:
+            for mZ in mktZ:
+                filter_dts = mktData.loc[np.abs(mktData.volZ) >= mZ, 'Date'].values
+                data = self.get_signals(data, filter_dts, **arg)
+                results, stats = self.calc_returns(data, max_gross, base_pos)
+                results.columns = [ind]
+                output_results = output_results.join(results, how='outer')
+                output_stats[ind] = stats
+                ind += 1
 
         self.write_index_results(output_results, index)
+        self.write_index_stats(output_stats, index)
 
     def create_features(self, df):
+        # Filter earnings announcements
+        secChange = data.SecCode != data.SecCode.shift(1)
+        ernRoll = df.EARNINGSFLAG.rolling(window=2,center=False).sum()
+        ernRoll[secChange] = 0.
+        df = df[ernRoll == 0.].copy()
+        
         # Create Gap measure and Z Measure
-        df['Ret'] = (df.AdjClose - df.AdjOpen) / df.AdjOpen
-
         df['GapDown'] = (df.AdjOpen - df.LAG1_AdjLow) / df.LAG1_AdjLow
         df['GapUp'] = (df.AdjOpen - df.LAG1_AdjHigh) / df.LAG1_AdjHigh
-
         df['zUp'] = df.GapUp / df.LAG1_VOL90_AdjClose
         df['zDown'] = df.GapDown / df.LAG1_VOL90_AdjClose
         
+        # Returns
+        df['Ret'] = (df.AdjClose - df.AdjOpen) / df.AdjOpen
         df['MktRet'] = (df.MKT_AdjClose - df.MKT_AdjOpen) / df.MKT_AdjOpen
         df['RetH'] = df.Ret - df.MktRet
 
         return df.reset_index(drop=True)
 
-    def get_trades(self, data, z):
-        dts = data.Date.unique()
-        #  Filter using Z and Momentum
-        data = data[data.EARNINGSFLAG == 0]
-        gapUpBelowMA = (data.zUp >= z) & (data.AdjOpen <= data.LAG1_MA20_AdjClose)
-        gapDwnAbvMA = (data.zDown <= -z) & (data.AdjOpen >= data.LAG1_MA20_AdjClose)
-        data = data.loc[gapUpBelowMA | gapDwnAbvMA]
-        data.sort_values('Date', inplace=True)
-        data.reset_index(drop=True, inplace=True)
+    def get_signals(self, data, filter_dts, z):
+        # Widen bands
+        upper = data.LAG1_MA20_AdjClose
+        lower = data.LAG1_MA20_AdjClose
 
-        # Trades and costs 
-        shorts = data.loc[(data.zUp > 0) & (data.GapUp > 0)].copy()
-        longs = data.loc[(data.zDown < 0) & (data.GapDown < 0)].copy()
+        # Get long/short signals
+        gapUpBelowMA = (data.zUp >= z) & (data.AdjOpen <= upper)
+        gapDwnAbvMA = (data.zDown <= -z) & (data.AdjOpen >= lower)
+        data['Short'] = np.where(gapUpBelowMA, 1, 0)
+        data['Long'] = np.where(gapDwnAbvMA, 1, 0)
+        
+        # Filter out market dts
+        data.loc[data.Date.isin(filter_dts), 'Short'] = 0
+        data.loc[data.Date.isin(filter_dts), 'Long'] = 0
+
+        return data
+
+    def calc_returns(self, data, max_gross, base_pos):
+        dts = data.Date.unique()
+        dts.sort()
+        longs = data[data.Long == 1].copy()
+        shorts = data[data.Short == 1].copy()
         shorts.RetH += COST
         longs.RetH -= COST
 
-        # Sum all trades
+        # Number of trades and mean Returns
+        longCounts = data.groupby('Date').Long.sum()
+        shortCounts = data.groupby('Date').Short.sum()
         longReturns = longs.groupby('Date').RetH.mean()
-        longCounts = longs.groupby('Date').RetH.count()
         shortReturns = shorts.groupby('Date').RetH.mean()
-        shortCounts = shorts.groupby('Date').RetH.count()
         
-        # Output df
-        output = pd.DataFrame(0, columns=['LongRet', 'nLong', 'ShortRet',
-                                          'nShort'], index = dts)
-        output.loc[longReturns.index, 'LongRet'] = longReturns
-        output.loc[longCounts.index, 'nLong'] = longCounts
-        output.loc[shortReturns.index, 'ShortRet'] = shortReturns
-        output.loc[shortCounts.index, 'nShort'] = shortCounts
-        output['TotRet'] = output.LongRet - output.ShortRet
+        # Determine position size per day
+        n_pos = longCounts + shortCounts
+        base_pos = base_pos * AUM
+        pos_size = np.where((max_gross/n_pos) < base_pos, (max_gross/n_pos),
+            base_pos)
 
-        return pd.DataFrame(output['TotRet'])
+        # Output df
+        output = pd.DataFrame(data={'nLong': longCounts,
+                                    'nShort': shortCounts,
+                                    'longRet': longReturns,
+                                    'shortRet': shortReturns},
+                              index = dts)
+        output.longRet.fillna(0., inplace=True)
+        output.shortRet.fillna(0., inplace=True)
+        output['longAlloc'] = output.nLong * pos_size
+        output['shortAlloc'] = output.nShort * -pos_size
+        output['longPL'] = output.longAlloc * output.longRet
+        output['shortPL'] = output.shortAlloc * output.shortRet
+        output['totalPL'] = output.longPL + output.shortPL
+
+        stats = {'nLong':longCounts.sum().astype(float),
+                 'nShort':shortCounts.sum().astype(float)}
+
+        return pd.DataFrame(output['totalPL'] / AUM), stats
 
     # ~~~~~~ DataConstructor params ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def get_features(self):
         '''
         Overriden method from Strategy
         '''
-        return ['AdjOpen', 'AdjHigh', 'AdjLow', 'AdjClose', 'LAG1_AdjHigh',
-                'LAG1_AdjLow', 'LAG1_VOL90_AdjClose', 'LAG1_MA20_AdjClose',
-                'EARNINGSFLAG', 'MKT_AdjClose', 'MKT_AdjOpen']
+        return ['AdjOpen', 'AdjClose', 'LAG1_AdjHigh', 'LAG1_AdjLow',
+                'LAG1_VOL90_AdjClose', 'LAG1_MA20_AdjClose', 'AvgDolVol',
+                'EARNINGSFLAG', 'MKT_AdjClose', 'MKT_AdjOpen',
+                'PRMAH5_AdjClose', 'PRMAH10_AdjClose', 'PRMAH20_AdjClose'
+                ]
 
     def get_filter_args(self):
         '''
@@ -100,7 +148,7 @@ class GapStrategy(Strategy):
         return {'filter': 'AvgDolVol',
                 'where': 'MarketCap >= 100 ' +
                 'and Close_ between 15 and 1000',
-                'univ_size': 800}
+                'univ_size': 500}
 
     def get_date_parameters(self):
         '''
@@ -130,15 +178,14 @@ def main():
         '-s', '--simulation', action='store_true',
         help='Run simulatoin')
     args = parser.parse_args()
-    
-    import ipdb; ipdb.set_trace()
+
     if args.data:
         GapStrategy().make_data()
     elif args.write_simulation:
-        strategy = GapStrategy('version_0001', True)
+        strategy = GapStrategy('version_0002', True)
         strategy.start()
     elif args.simulation:
-        strategy = GapStrategy('version_0001', False)
+        strategy = GapStrategy('version_0002', False)
         strategy.start()
 
 
