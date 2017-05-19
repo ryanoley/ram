e2616d90de1d3467101dae8658c3955fc09786d0
Mitchell Suter <mitchellasuter@gmail.com>
2017-05-19 13:42:25 -0400

    ADD: earnings starmine arm

--- data/ern_pead/research/technical_vars_4.sql
+++ data/ern_pead/research/technical_vars_4.sql
@@ -8,26 +8,53 @@ select * from ram.dbo.ram_pead_report_dates where $(trade) = 2
 )
 
 
-, typical_prices1 as (
+, prices1 as (
 select			*,
+				-- MFI
 				(AdjHigh + AdjLow + AdjClose) / 3 as TypPrice,
-				(AdjHigh + AdjLow + AdjClose) / 3 * AdjVolume as MonFlowP
+				(AdjHigh + AdjLow + AdjClose) / 3 * AdjVolume as MonFlowP,
+				-- Trade Vs Shares Out
+				MarketCap / AdjClose * 1e6 as Shares,
+				avg(AdjVolume) over (
+					partition by SecCode
+					order by Date_
+					rows between 9 preceding and current row) as AvgVolume10,
+				avg(AdjVolume) over (
+					partition by SecCode
+					order by Date_
+					rows between 29 preceding and current row) as AvgVolume30,
+				avg(AdjVolume) over (
+					partition by SecCode
+					order by Date_
+					rows between 59 preceding and current row) as AvgVolume60
+
 from			ram.dbo.ram_equity_pricing_research
 where			SecCode in (select distinct SecCode from ram.dbo.ram_master_ids)
 )
 
 
-, typical_prices2 as (
+, prices2 as (
 select			*,
+				-- MFI
 				lag(TypPrice, 1) over (
 					partition by SecCode
-					order by Date_) as LagTypPrice
-from			typical_prices1
+					order by Date_) as LagTypPrice,
+				-- Trade Vs Shares Out
+				AvgVolume30 / Shares as TrdVsOut,
+				lag(AvgVolume30 / Shares, 1) over (
+					partition by SecCode
+					order by Date_) as LagTrdVsOut,
+				-- Trends in volumes
+				AvgVolume10 / AvgVolume30 as VolumeMA10_30,
+				AvgVolume10 / AvgVolume60 as VolumeMA10_60
+
+from			prices1
 )
 
 
-, money_flow as (
+, prices3 as (
 select			*,
+				-- MFI
 				case 
 					when TypPrice > LagTypPrice then MonFlowP
 					else 0 
@@ -36,13 +63,17 @@ select			*,
 					when LagTypPrice is null then MonFlowP
 					when TypPrice != LagTypPrice then MonFlowP 
 					else 0 
-				end as MonFlow
-from			typical_prices2
+				end as MonFlow,
+				-- Trade Vs Shares Out
+			    100 * (TrdVsOut - LagTrdVsOut) / LagTrdVsOut as DelTrdVsOut
+from			prices2
+
 )
 
 
-, mfi1 as (
+, prices4 as (
 select			*,
+				-- MFI
 				sum(MonFlow_P) over (
 					partition by SecCode
 					order by Date_ 
@@ -69,7 +100,7 @@ select			*,
 					partition by SecCode
 					order by Date_
 					rows between 29 preceding and current row), 0) * 100 as MFI_30
-from			money_flow
+from			prices3
 where			NormalTradingFlag = 1
 	and			OneYearTradingFlag = 1
 	and			AvgDolVol >= 3
@@ -80,14 +111,14 @@ where			NormalTradingFlag = 1
 )
 
 
-, mfi2 as (
+, prices5 as (
 select			SecCode,
 				Date_,
 
+				-- MFI
 				MFI_5,
 				MFI_10,
 				MFI_30,
-
 				percent_rank() over (
 					partition by Date_
 					order by MFI_5) as MFI_5_Rank,
@@ -96,23 +127,52 @@ select			SecCode,
 					order by MFI_10) as MFI_10_Rank,
 				percent_rank() over (
 					partition by Date_
-					order by MFI_30) as MFI_30_Rank
+					order by MFI_30) as MFI_30_Rank,
+				-- Trade Vs Shares Out
+				TrdVsOut,
+				DelTrdVsOut,
+				percent_rank() over (
+					partition by Date_
+					order by TrdVsOut) as TrdVsOut_Rank,
+				percent_rank() over (
+					partition by Date_
+					order by DelTrdVsOut) as DelTrdVsOut_Rank,
+				-- Trends in Volumes
+				VolumeMA10_30,
+				VolumeMA10_60,
+				percent_rank() over (
+					partition by Date_
+					order by VolumeMA10_30) as VolumeMA10_30_Rank,
+				percent_rank() over (
+					partition by Date_
+					order by VolumeMA10_60) as VolumeMA10_60_Rank				
 
-from			mfi1
+from			prices4
 )
 
 
 select			A.SecCode,
 				A.ReportDate,
+				-- MFI
 				B.MFI_5,
 				B.MFI_10,
 				B.MFI_30,
 				B.MFI_5_Rank,
 				B.MFI_10_Rank,
-				B.MFI_30_Rank
+				B.MFI_30_Rank,
+				-- Trade Vs Shares Out
+				B.TrdVsOut,
+				B.DelTrdVsOut,
+				B.TrdVsOut_Rank,
+				B.DelTrdVsOut_Rank,
+				-- Volume Trends
+				B.VolumeMA10_30,
+				B.VolumeMA10_60,
+				B.VolumeMA10_30_Rank,
+				B.VolumeMA10_60_Rank
 
 from			report_dates A
-	join		mfi2 B
+	join		prices5 B
 		on		A.SecCode = B.SecCode
 		and		A.FilterDate = B.Date_
 
