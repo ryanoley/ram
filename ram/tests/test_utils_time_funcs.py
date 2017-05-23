3f6d0decf22384c4b4e9d1d1895b23866ae9f0b5
mitchellsuter <mitchellasuter@gmail.com>
2017-05-23 09:28:49 -0400

    ADD: convert_date_array for ram

--- ram/tests/test_utils_time_funcs.py
+++ ram/tests/test_utils_time_funcs.py
@@ -1,7 +1,10 @@
 import unittest
+import numpy as np
 import datetime as dt
 
-from ram.utils.time_funcs import check_input_date
+from numpy.testing import assert_array_equal
+
+from ram.utils.time_funcs import check_input_date, convert_date_array
 
 
 class TestTimeFuncs(unittest.TestCase):
@@ -25,6 +28,23 @@ class TestTimeFuncs(unittest.TestCase):
         result = check_input_date('01/20/93 10:20:30')
         benchmark = dt.datetime(1993, 1, 20)
         self.assertEqual(result, benchmark)
+
+    def test_convert_date_array(self):
+        dates = ['2010-01-01', '2010-02-01']
+        result = convert_date_array(dates)
+        benchmark = np.array([dt.datetime(2010, 1, 1),
+                              dt.datetime(2010, 2, 1)])
+        assert_array_equal(result, benchmark)
+        dates = [dt.date(2010, 1, 1), dt.date(2010, 2, 1)]
+        result = convert_date_array(dates)
+        benchmark = np.array([dt.datetime(2010, 1, 1),
+                              dt.datetime(2010, 2, 1)])
+        assert_array_equal(result, benchmark)
+        dates = ['20100101', '20100201']
+        result = convert_date_array(dates)
+        benchmark = np.array([dt.datetime(2010, 1, 1),
+                              dt.datetime(2010, 2, 1)])
+        assert_array_equal(result, benchmark)
 
     def tearDown(self):
         pass
