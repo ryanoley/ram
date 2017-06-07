5c2adc0e661354565da8abeff03e29a77fcb493d
mitchellsuter <mitchellasuter@gmail.com>
2017-06-07 09:43:29 -0400

    MOD: config updated with temp ern pead directory

--- ram/config.py
+++ ram/config.py
@@ -5,3 +5,5 @@ BASE_DIR = os.path.join(os.getenv('DATA'), 'ram')
 PREPPED_DATA_DIR = os.path.join(BASE_DIR, 'prepped_data')
 
 SIMULATION_OUTPUT_DIR = os.path.join(BASE_DIR, 'simulations')
+
+ERN_PEAD_DIR = os.path.join(os.getenv('DATA'), 'ram', 'data', 'temp_ern_pead')
