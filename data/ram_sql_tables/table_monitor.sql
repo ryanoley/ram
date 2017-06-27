0a2d6206c4335a77f0a6ea519cc202009c60cde0
RAMServer <analysts@roundaboutam.com>
2017-06-27 11:19:14 -0400

    BUG: fixed table monitor issue

--- data/ram_sql_tables/table_monitor.sql
+++ data/ram_sql_tables/table_monitor.sql
@@ -1,4 +1,3 @@
-
 use ram;
 
 -- ######  Final Accounting Table table   #########################################
@@ -24,7 +23,7 @@ declare @StatusDateTime smalldatetime = getdate();
 
 
 ; with table_ram_annualized_cash_dividends as (
-select			@StatusDateTime as MonitorDateTime,
+select			@StatusDateTime as StatusDateTime,
 				'ram_annualized_cash_dividends' as TableName,
 				min(ExDate) as MinTableDate,
 				max(ExDate) as MaxTableDate,
@@ -34,7 +33,7 @@ from			ram.dbo.ram_annualized_cash_dividends
 
 
 , table_ram_compustat_accounting as (
-select			@StatusDateTime as MonitorDateTime,
+select			@StatusDateTime as StatusDateTime,
 				'ram_compustat_accounting' as TableName,
 				min(ReportDate) as MinTableDate,
 				max(ReportDate) as MaxTableDate,
@@ -44,7 +43,7 @@ from			ram.dbo.ram_compustat_accounting
 
 
 , table_ram_compustat_accounting_derived as (
-select			@StatusDateTime as MonitorDateTime,
+select			@StatusDateTime as StatusDateTime,
 				'ram_compustat_accounting_derived' as TableName,
 				min(AsOfDate) as MinTableDate,
 				max(AsOfDate) as MaxTableDate,
@@ -54,7 +53,7 @@ from			ram.dbo.ram_compustat_accounting_derived
 
 
 , table_ram_compustat_sector as (
-select			@StatusDateTime as MonitorDateTime,
+select			@StatusDateTime as StatusDateTime,
 				'ram_compustat_sector' as TableName,
 				min(StartDate) as MinTableDate,
 				max(EndDate) as MaxTableDate,
@@ -64,7 +63,7 @@ from			ram.dbo.ram_compustat_sector
 
 
 , table_ram_dividend_yield as (
-select			@StatusDateTime as MonitorDateTime,
+select			@StatusDateTime as StatusDateTime,
 				'ram_dividend_yield' as TableName,
 				min(Date_) as MinTableDate,
 				max(Date_) as MaxTableDate,
@@ -74,7 +73,7 @@ from			ram.dbo.ram_dividend_yield
 
 
 , table_ram_equity_pricing as (
-select			@StatusDateTime as MonitorDateTime,
+select			@StatusDateTime as StatusDateTime,
 				'ram_equity_pricing' as TableName,
 				min(Date_) as MinTableDate,
 				max(Date_) as MaxTableDate,
@@ -84,7 +83,7 @@ from			ram.dbo.ram_equity_pricing
 
 
 , table_ram_equity_pricing_research as (
-select			@StatusDateTime as MonitorDateTime,
+select			@StatusDateTime as StatusDateTime,
 				'ram_equity_pricing_research' as TableName,
 				min(Date_) as MinTableDate,
 				max(Date_) as MaxTableDate,
@@ -94,7 +93,7 @@ from			ram.dbo.ram_equity_pricing_research
 
 
 , table_ram_equity_report_dates as (
-select			@StatusDateTime as MonitorDateTime,
+select			@StatusDateTime as StatusDateTime,
 				'ram_equity_report_dates' as TableName,
 				min(ReportDate) as MinTableDate,
 				max(ReportDate) as MaxTableDate,
@@ -104,7 +103,7 @@ from			ram.dbo.ram_equity_report_dates
 
 
 , table_ram_index_pricing as (
-select			@StatusDateTime as MonitorDateTime,
+select			@StatusDateTime as StatusDateTime,
 				'ram_index_pricing' as TableName,
 				min(Date_) as MinTableDate,
 				max(Date_) as MaxTableDate,
@@ -114,7 +113,7 @@ from			ram.dbo.ram_index_pricing
 
 
 , table_ram_master_ids as (
-select			@StatusDateTime as MonitorDateTime,
+select			@StatusDateTime as StatusDateTime,
 				'ram_master_ids' as TableName,
 				min(StartDate) as MinTableDate,
 				max(EndDate) as MaxTableDate,
@@ -124,7 +123,7 @@ from			ram.dbo.ram_master_ids
 
 
 , table_ram_master_ids_etf as (
-select			@StatusDateTime as MonitorDateTime,
+select			@StatusDateTime as StatusDateTime,
 				'ram_master_ids_etf' as TableName,
 				min(StartDate) as MinTableDate,
 				max(EndDate) as MaxTableDate,
@@ -132,9 +131,17 @@ select			@StatusDateTime as MonitorDateTime,
 from			ram.dbo.ram_master_ids_etf
 )
 
+, table_ram_etf_pricing as (
+select			@StatusDateTime as StatusDateTime,
+				'ram_etf_pricing' as TableName,
+				min(Date_) as MinTableDate,
+				max(Date_) as MaxTableDate,
+				count(*) as RowCount_
+from			ram.dbo.ram_etf_pricing
+)
 
 , table_ram_starmine_map as (
-select			@StatusDateTime as MonitorDateTime,
+select			@StatusDateTime as StatusDateTime,
 				'ram_starmine_map' as TableName,
 				min(StartDate) as MinTableDate,
 				max(EndDate) as MaxTableDate,
@@ -144,7 +151,7 @@ from			ram.dbo.ram_starmine_map
 
 
 , table_ram_trading_dates as (
-select			@StatusDateTime as MonitorDateTime,
+select			@StatusDateTime as StatusDateTime,
 				'ram_trading_dates' as TableName,
 				min(CalendarDate) as MinTableDate,
 				max(CalendarDate) as MaxTableDate,
@@ -155,13 +162,118 @@ from			ram.dbo.ram_trading_dates
 
 --  Created in separate directory
 , table_ram_idccode_to_gvkey_map as (
-select			@StatusDateTime as MonitorDateTime,
+select			@StatusDateTime as StatusDateTime,
 				'ram_idccode_to_gvkey_map' as TableName,
 				min(StartDate) as MinTableDate,
 				max(EndDate) as MaxTableDate,
 				count(*) as RowCount_
 from			ram.dbo.ram_idccode_to_gvkey_map
 )
+
+
+-- Legacy Tables
+-- univ_filter_data
+, univ_filter as (
+select 
+    @StatusDateTime as StatusDateTime,
+    'univ_filter_data' as TableName,
+    min(Date_) as MinTableDate,
+    max(Date_) as MaxTableDate,
+    count(*) as Count_
+from ram.dbo.univ_filter_data
+)
+
+-- univ_filter_data_etf
+, univ_filter_etf as (
+select 
+    @StatusDateTime as StatusDateTime,
+    'univ_filter_data_etf' as TableName,
+    min(Date_) as MinTableDate,
+    max(Date_) as MaxTableDate,
+    count(*) as Count_
+from ram.dbo.univ_filter_data_etf
+)
+
+-- ern_live
+, ern_live as (
+select 
+    @StatusDateTime as StatusDateTime,
+    'ern_event_dates_live' as TableName,
+    min(EvalDate) as MinTableDate,
+    max(EvalDate) as MaxTableDate,
+    count(*) as Count_
+from ram.dbo.ern_event_dates_live
+)
+
+-- pead_live
+, pead_live as (
+select 
+    @StatusDateTime as StatusDateTime,
+    'pead_event_dates_live' as TableName,
+    min(EvalDate) as MinTableDate,
+    max(EvalDate) as MaxTableDate,
+    count(*) as Count_
+from ram.dbo.pead_event_dates_live
+)
+
+-- report_dates
+, report_dates as (
+select 
+    @StatusDateTime as StatusDateTime,
+    'report_dates' as TableName,
+    min(ReportDate) as MinTableDate,
+    max(ReportDate) as MaxTableDate,
+    count(*) as Count_
+from ram.dbo.report_dates
+)
+
+-- short_interest
+, short_int as (
+select 
+    @StatusDateTime as StatusDateTime,
+    'ShortInterest' as TableName,
+    min(Date_) as MinTableDate,
+    max(Date_) as MaxTableDate,
+    count(*) as Count_
+from ram.dbo.ShortInterest
+)
+
+-- Starmine ARM
+, sm_arm as (
+select 
+    @StatusDateTime as StatusDateTime,
+    'sm_ARM' as TableName,
+    min(AsOfDate) as MinTableDate,
+    max(AsOfDate) as MaxTableDate,
+    count(*) as Count_
+from ram.dbo.sm_ARM
+)
+
+-- Starmine ShortInterest
+, sm_si as (
+select 
+    @StatusDateTime as StatusDateTime,
+    'sm_ShortInterest' as TableName,
+    min(AsOfDate) as MinTableDate,
+    max(AsOfDate) as MaxTableDate,
+    count(*) as Count_
+from ram.dbo.sm_ShortInterest
+)
+
+
+-- Starmine SmartEstimate
+, sm_se_eps as (
+select 
+    @StatusDateTime as StatusDateTime,
+    'sm_SmartEstimate_eps' as TableName,
+    min(AsOfDate) as MinTableDate,
+    max(AsOfDate) as MaxTableDate,
+    count(*) as Count_
+from ram.dbo.sm_SmartEstimate_eps
+)
+
+
+
 
 
 insert into ram.dbo.ram_table_monitor
@@ -187,8 +299,28 @@ select * from table_ram_master_ids
 union
 select * from table_ram_master_ids_etf
 union
+select * from table_ram_etf_pricing
+union
 select * from table_ram_starmine_map
 union
 select * from table_ram_trading_dates
 union
 select * from table_ram_idccode_to_gvkey_map
+union
+select * from univ_filter
+union
+select * from univ_filter_etf
+union
+select * from ern_live
+union
+select * from pead_live
+union
+select * from report_dates
+union
+select * from short_int
+union
+select * from sm_arm
+union
+select * from sm_si
+union
+select * from sm_se_eps
