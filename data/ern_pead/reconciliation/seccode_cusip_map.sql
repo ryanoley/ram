e14213e8ea5a254e93e2c0fe2080d953df12d236
Mitchell Suter <mitchellasuter@gmail.com>
2017-07-05 15:29:57 -0400

    BUG: ern reconciliation seccode cusip map needs dates and different filter; commented out portfolio manager copy from server due to error

--- data/ern_pead/reconciliation/seccode_cusip_map.sql
+++ data/ern_pead/reconciliation/seccode_cusip_map.sql
@@ -1,4 +1,5 @@
 SET NOCOUNT ON
 
-select distinct SecCode, Ticker, Cusip from ram.dbo.ram_master_ids
-where SecCode in (select distinct SecCode from ram.dbo.ram_equity_pricing where Date_ > dateadd(day, -180, getdate()))
+select distinct SecCode, Ticker, Cusip, StartDate, EndDate from ram.dbo.ram_master_ids
+where EndDate >= dateadd(day, -180, getdate())
+and SecCode in (select distinct SecCode from ram.dbo.ram_equity_pricing_research)
