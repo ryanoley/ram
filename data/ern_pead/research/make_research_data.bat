dfe496ba0e04d797552e4f5d1581a98d513ec9da
mitchellsuter <mitchellasuter@gmail.com>
2017-07-06 10:59:06 -0400

    BUG: make research data should write to ram data dir

--- data/ern_pead/research/make_research_data.bat
+++ data/ern_pead/research/make_research_data.bat
@@ -2,8 +2,8 @@ set SQLCMD="%SQLCMDPATH%\sqlcmd" -S 192.168.2.8 -d ram -U ramuser -P 183madison 
 
 set SCRIPTS=%GITHUB%\ram\data\ern_pead\research\
 
-set OUTPUTS_ERN=%DATA%\earnings\implementation\reconcile
-set OUTPUTS_PEAD=%DATA%\pead\implementation\reconcile
+set OUTPUTS_ERN=%DATA%\ram\data\temp_ern_pead\earnings
+set OUTPUTS_PEAD=%DATA%\ram\data\temp_ern_pead\pead
 
 :: Report Dates for earnings and post earnings put into table
 %SQLCMD% -v trade=1 -i %GITHUB%\ram\data\ern_pead\universe_filter_report_dates.sql
