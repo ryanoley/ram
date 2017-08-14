ad5e65e7df43908a0ea84d9b853607fdddca9cc5
mitchellsuter <mitchellasuter@gmail.com>
2017-08-14 17:52:35 -0400

    MOD: constructor2 for market cap

--- ram/strategy/long_pead/constructor/constructor2.py
+++ ram/strategy/long_pead/constructor/constructor2.py
@@ -5,32 +5,46 @@ import datetime as dt
 from ram.strategy.long_pead.utils import make_variable_dict
 
 from ram.strategy.long_pead.constructor.portfolio import Portfolio
-from ram.strategy.long_pead.constructor.constructor1 import PortfolioConstructor1
+from ram.strategy.long_pead.constructor.base_constructor import Constructor
 
 
-class PortfolioConstructor2(PortfolioConstructor1):
+class PortfolioConstructor2(Constructor):
 
     def get_args(self):
         return {
             'logistic_spread': [0.01, 0.1, 0.5, 1]
         }
 
-    def get_position_sizes(self, scores, logistic_spread, groups):
-
+    def get_position_sizes(self, scores, logistic_spread, n_groups):
+        # Output should have all the same keys, but can return nan values
+        output_sizes = {x: np.nan for x in scores.keys()}
+        # Reformat input
         scores = pd.Series(scores, name='score').to_frame().join(
-            pd.Series(self.market_cap, name='market_cap'))
+            pd.Series(self.market_cap, name='market_cap')).dropna()
+        # Sort on market cap
+        scores = scores.sort_values('market_cap')
+        scores['group'] = np.arange(len(scores)) / n_groups
+        # Get unique groups and their proportions. This is used for
+        # total allocation to group.
+        groups = scores.groupby('group')['score'].count()
+        groups = groups / float(groups.sum())
+        # Collect weighted output that has been scaled already.
+        output = pd.DataFrame()
+        for g, prop in groups.iteritems():
+            output = output.append(weight_group(
+                scores[scores.group == g], logistic_spread, prop))
+        # Put data into output dictionary for downstream. Preserves nans
+        for key in output_sizes.keys():
+            output_sizes[key] = output.weights.loc[key]
+        return output_sizes
 
-        scores = scores.sort_values('score')
 
-
-        # Simple rank
-        def logistic_weight(k):
-            return 2 / (1 + np.exp(-k)) - 1
-
-        n_good = (~scores.score.isnull()).sum()
-        n_bad = scores.score.isnull().sum()
-        scores['weights'] = [
-            logistic_weight(x) for x in np.linspace(
-                -logistic_spread, logistic_spread, n_good)] + [0] * n_bad
-        scores.weights = scores.weights / scores.weights.abs().sum()
-        return scores.weights.to_dict()
+def weight_group(data, logistic_spread, total_gross):
+    def logistic_weight(k):
+        return 2 / (1 + np.exp(-k)) - 1
+    data = data.sort_values('score')
+    data['weights'] = [
+        logistic_weight(x) for x in np.linspace(
+            -logistic_spread, logistic_spread, len(data))]
+    data.weights = data.weights / data.weights.abs().sum() * total_gross
+    return data
--- ram/strategy/long_pead/tests/test_constructor2.py
+++ ram/strategy/long_pead/tests/test_constructor2.py
@@ -19,8 +19,18 @@ class TestConstructor2(unittest.TestCase):
         cons = PortfolioConstructor2()
         cons.market_cap = {'AAPL': 10, 'IBM': 20, 'BAC': 30, 'GS': 50}
         scores = {'AAPL': 4, 'IBM': 10, 'BAC': 4, 'GS': -10}
-        import pdb; pdb.set_trace()
         result = cons.get_position_sizes(scores, 0.1, 2)
+        benchmark = {'AAPL': -0.25, 'GS': -0.25, 'IBM': 0.25, 'BAC': 0.25}
+        self.assertDictEqual(result, benchmark)
+
+    def test_weight_group(self):
+        data = pd.DataFrame({'score': [4, 2, 3]}, index=['AAPL', 'GS', 'IBM'])
+        result = weight_group(data, 1, 10)
+        benchmark = data.copy()
+        benchmark = pd.DataFrame({'score': [2, 3, 4]},
+                                 index=['GS', 'IBM', 'AAPL'])
+        benchmark['weights'] = [-5, 0, 5.]
+        assert_frame_equal(result, benchmark)
 
     def tearDown(self):
         pass
