e9f2ecf4040cafd94bf5ecac935837a2608376d7
ryanoley <ryan@roundaboutam.com>
2017-08-16 09:13:30 -0400

    MOD: Fix log time output in batch

--- data/ram_sql_tables/starmine/data_starmine.bat
+++ data/ram_sql_tables/starmine/data_starmine.bat
@@ -6,7 +6,7 @@
 ::	Environment variables should be set on client machine to the DATA directory
 ::	on the MarkertQA server and the local Git repo.
 
-SETLOCAL ENABLEEXTENSIONS
+SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
 set SCRIPTDIR=%GITHUB%\ram\data\ram_sql_tables\starmine
 set LOGFILE=%LOGDIR%\starmine.log
@@ -14,11 +14,11 @@ set LOGFILE=%LOGDIR%\starmine.log
 
 >> %LOGFILE% 2>&1(
 echo -------------------------
-echo DATA - StarMine - %date%_%time% - Start
+echo DATA - StarMine - %date%_!time! - Start
 
 python "%SCRIPTDIR%\starmine_ftp.py"
 
-echo DATA - StarMine - %date%_%time% - Complete
+echo DATA - StarMine - %date%_!time! - Complete
 echo:
 echo:
 echo:
