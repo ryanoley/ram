2863e0bec9ec0c2f0ad6fe0170aed6fe8612c688
mitchellsuter <mitchellasuter@gmail.com>
2017-09-01 17:08:05 -0400

    MOD: update for weekly simulation

--- ram/strategy/long_pead/main.py
+++ ram/strategy/long_pead/main.py
@@ -18,7 +18,7 @@ from ram.strategy.long_pead.signals.signals2 import SignalModel2
 
 class LongPeadStrategy(Strategy):
 
-    data = DataContainer2()
+    data = DataContainer1()
     signals = SignalModel1()
     constructor = PortfolioConstructor2()
 
@@ -41,9 +41,6 @@ class LongPeadStrategy(Strategy):
         return output_params
 
     def run_index(self, time_index):
-
-        if time_index < 20:
-            return None
 
         # Attach market data first since it is merged with equity data
         self.data.add_market_data(self.read_market_index_data())
