7920702ae57a144f559f881d79dc57fd732eeb6b
RAMServer <analysts@roundaboutam.com>
2017-11-07 14:07:04 -0500

    BUGS: daily update etl issues

--- data/ram_sql_tables/daily_update.bat
+++ data/ram_sql_tables/daily_update.bat
@@ -1,33 +1,64 @@
-set SQLCMD="%SQLCMDPATH%\sqlcmd" -S 192.168.2.8 -d ram -U ramuser -P 183madison
 
+@echo off
+
+SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+
+set SQLCMD="%SQLCMDPATH%\sqlcmd" -S 192.168.2.8 -d ram -U ramuser -P 183madison
+set LOGFILE=%LOGDIR%\refresh_ram_tables.log
 set SQLDIR=%GITHUB%\ram\data\ram_sql_tables
 
-:: Master ID Tables
+>> %LOGFILE% 2>&1(
+echo --------------------------------
+echo REFRESH - RAM Daily Tables - %date%_!time! - Start
+
+:: Master ID Tabless
+echo. & echo ~~ ram_master_ids !time!~~
 %SQLCMD% -i %SQLDIR%\ram_master_ids.sql
+
+echo. & echo  ~~ ram_master_ids_etf !time!~~
 %SQLCMD% -i %SQLDIR%\ram_master_ids_etf.sql
 
 :: RAM Tables
-
+echo. & echo ~~ ram_annualized_cash_dividends !time!~~
 %SQLCMD% -i %SQLDIR%\ram_annualized_cash_dividends.sql
 
+echo. & echo ~~ ram_compustat_accounting !time!~~
 %SQLCMD% -i %SQLDIR%\ram_compustat_accounting.sql
+
+echo. & echo ~~ ram_compustat_accounting_derived !time!~~
 %SQLCMD% -i %SQLDIR%\ram_compustat_accounting_derived.sql
+
+echo. & echo ~~ ram_compustat_sector !time!~~
 %SQLCMD% -i %SQLDIR%\ram_compustat_sector.sql
 
+echo. & echo ~~ ram_dividend_yield !time!~~
 %SQLCMD% -i %SQLDIR%\ram_dividend_yield.sql
 
+echo. & echo ~~ ram_equity_pricing_etf !time!~~
 %SQLCMD% -v tabletype=1 -i %SQLDIR%\ram_equity_pricing.sql
+
+echo. & echo ~~ ram_equity_pricing !time!~~
 %SQLCMD% -v tabletype=2 -i %SQLDIR%\ram_equity_pricing.sql
+
+echo. & echo ~~ ram_equity_pricing_research !time!~~
 %SQLCMD% -i %SQLDIR%\ram_equity_pricing_research.sql
 
+echo. & echo ~~ ram_equity_report_dates !time!~~
 %SQLCMD% -i %SQLDIR%\ram_equity_report_dates.sql
 
+echo. & echo ~~ ram_index_pricing !time!~~
 %SQLCMD% -i %SQLDIR%\ram_index_pricing.sql
 
+echo. & echo ~~ ram_starmine_map !time!~~
 %SQLCMD% -i %SQLDIR%\starmine\ram_starmine_map.sql
 
 :: Call GVKEY Mapping Batch File
+echo. & echo ~~ get_map_data !time!~~
 call %SQLDIR%\gvkey_idc_mapping\get_map_data.bat
 
+echo. & echo ~~ table_monitor !time!~~
 %SQLCMD% -i %SQLDIR%\table_monitor.sql
+
+echo. & echo REFRESH - RAM Daily Tables - %date%_!time! - End
+)
 
--- tasks/etl_early_morning.bat
+++ tasks/etl_early_morning.bat
@@ -1,11 +1,10 @@
 
-set SQLCMD="%SQLCMDPATH%\sqlcmd" -S 192.168.2.8 -d ram -U ramuser -P 183madison
+@echo off
+
+SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
 :: RAM TABLES
 call %GITHUB%\ram\data\ram_sql_tables\daily_update.bat
-
-:: Check status of QAD and RAM tables and send alerts if necessary
-python %GITHUB%\ram\tasks\db_monitor.py -update_db_stats -check_qad
 
 :: Position sheet scraper
 python %GITHUB%\ram\tasks\position_sheet_scraper.py
