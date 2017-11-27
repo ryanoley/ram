290cbb0825dbe0bcf845d71d8da3c026ca635121
mitchellsuter <mitchellasuter@gmail.com>
2017-11-27 10:49:06 -0500

    ADD: statarb current top params to implementation dir'

--- ram/strategy/statarb/implementation/preprocess_new_models.py
+++ ram/strategy/statarb/implementation/preprocess_new_models.py
@@ -6,19 +6,27 @@ from ram import config
 from ram.strategy.statarb.statarb_config import implementation_top_models
 from ram.data.data_constructor_blueprint import DataConstructorBlueprint
 
-from ram.strategy.base import read_json, write_json, clean_top_params
+from ram.strategy.base import read_json, write_json
 
 
 def main():
 
     output_dir = check_implementation_folder_structure()
 
-    top_params = clean_top_params(implementation_top_models)
+    top_params = import_current_top_params()
 
-    for i, (param, run) in enumerate(zip(*top_params)):
-        # Read meta
-        run_meta = get_run_meta(run)
-        data_version = run_meta['prepped_data_version']
+    data_versions = []
+    for key, params in top_params.iteritems():
+
+        # Remove column index from run name
+        run_name = key[:key.rfind('_')]
+
+        # Get data version
+        data_version = params['prepped_data_version']
+        if data_version in data_versions:
+            continue
+        data_versions.append(data_version)
+
         # Locate seccodes
         seccodes = get_univ_seccodes(data_version)
         # Create blueprint for each unique data version
@@ -27,7 +35,7 @@ def main():
         # Create and write
         blueprint = DataConstructorBlueprint(strategy_name='StatArbStrategy',
                                              constructor_type='seccodes',
-                                             description=run)
+                                             description=run_name)
 
         blueprint.features = data_meta['blueprint']['features']
         blueprint.seccodes_filter_arguments['seccodes'] = seccodes
@@ -38,6 +46,9 @@ def main():
             output_dir, 'current_blueprint_{}.json'.format(data_version))
 
         write_json(blueprint.to_json(), blueprint_path)
+    preprocess_name = os.path.basename(output_dir)
+    print('\nUpdate `statarb_config/preprocessed_data_dir` with preprocess '
+          'version: {}\n'.format(preprocess_name))
 
 
 def check_implementation_folder_structure(
@@ -65,6 +76,11 @@ def check_implementation_folder_structure(
     return new_path
 
 
+def import_current_top_params(
+        top_params_dir=os.path.dirname(os.path.abspath(__file__))):
+    return read_json(os.path.join(top_params_dir, 'current_top_params.json'))
+
+
 def get_univ_seccodes(version, prepped_data_dir=config.PREPPED_DATA_DIR):
     # Get most recent seccodes from data file. This will be the live
     # universe
@@ -81,12 +97,6 @@ def get_univ_seccodes(version, prepped_data_dir=config.PREPPED_DATA_DIR):
 def get_prepped_data_meta(version, prepped_data_dir=config.PREPPED_DATA_DIR):
     path = os.path.join(prepped_data_dir, 'StatArbStrategy',
                         version, 'meta.json')
-    return read_json(path)
-
-
-def get_run_meta(run_name, simulation_dir=config.SIMULATIONS_DATA_DIR):
-    path = os.path.join(simulation_dir, 'StatArbStrategy',
-                        run_name, 'meta.json')
     return read_json(path)
 
 
