0099d672de1e0f7e4298e92053dd2ea6788c56e0
mitchellsuter <mitchellasuter@gmail.com>
2017-12-18 09:42:26 -0500

    MOD

--- tasks/statarb_pretrade_data_check.bat
+++ tasks/statarb_pretrade_data_check.bat
@@ -1,21 +1,13 @@
 @echo off
-SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
 set SCRIPTDIR=%GITHUB%\ram\ram\strategy\statarb\implementation\
-set LOGFILE=%LOGDIR%\statarb_data_check_log.log
 
 echo ------------------------------
 echo --  Data check for StatArb  --
 echo ------------------------------
 
->> %LOGFILE% 2>&1(
-echo ---------------------------------
-echo Daily data pull - %date%_!time! - Start
-
 python %SCRIPTDIR%\check_data.py
 
-echo Daily data pull - %date%_!time! - End
-echo:
-)
-
 START %DATA%\ram\implementation\StatArbStrategy\pretrade_data_check.csv
+
+pause
