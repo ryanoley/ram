23230ec937a2baf223338f018da76690fa6bb120
Mitchell Suter <mitchellasuter@gmail.com>
2018-01-11 18:10:53 -0500

    ADD: gvkey mapping scripts

---
+++ data/ram_sql_tables/gvkey_mapping/copy_raw_table.sql
@@ -0,0 +1,32 @@
+
+-------------------------------------------------------------
+-- Create tables
+
+/*
+if object_id('ram.dbo.ram_compustat_pit_map_raw', 'U') is not null 
+	drop table ram.dbo.ram_compustat_pit_map_raw
+
+
+create table	ram.dbo.ram_compustat_pit_map_raw (
+				GVKey int,
+				Changedate smalldatetime,
+				Name_ varchar(45),
+				Ticker varchar(15),
+				Exchange smallint,
+				Cusip varchar(15),
+				TableName varchar(15),
+				primary key (GVKey, Changedate, TableName)
+)
+
+
+; with all_data as (
+select *, 'CSPITId' as TableName from qai.dbo.CSPITId
+union
+select *, 'CSPITIdC' as TableName from qai.dbo.CSPITIdC
+)
+
+insert into ram.dbo.ram_compustat_pit_map_raw
+select * from all_data
+order by GVKey, Changedate
+
+*/
