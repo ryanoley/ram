48be4c7f5d2d1088439add62d3414716c73178cb
Mitchell Suter <mitchellasuter@gmail.com>
2018-01-22 15:02:16 -0500

    MOD

--- data/ram_sql_tables/gvkey_mapping/process_raw_tables.sql
+++ data/ram_sql_tables/gvkey_mapping/process_raw_tables.sql
@@ -6,35 +6,64 @@ NOTES:
 
 */
 
+select * from ram.dbo.ram_compustat_pit_map_raw
+where GVKey in (6268, 10787)
 
--- Get max changedate and Ticker cusip values
-; with last_entry_by_gvkey as (
-select		A.*
-from		ram.dbo.ram_compustat_pit_map_raw A
-join	(	select GVKey, max(Changedate) as MaxChangeDate 
-			from ram.dbo.ram_compustat_pit_map_raw
-			group by GVKey
-		) B
-	on		A.GVKey = B.GVKey
-	and		A.Changedate = B.MaxChangeDate
+
+
+; with X as (
+select GVKey, Changedate, substring(Cusip, 0, 9) as Cusip, SecIntCode, 0 as Source_ from ram.dbo.ram_compustat_pit_map_us
+union
+select GVKey, AsOfDate as Changedate, Cusip, SecIntCode, 1 as Source_ from ram.dbo.ram_compustat_csvsecurity_map_raw
+where EXCNTRY = 'USA'
+union
+select GVKey, AsOfDate as Changedate, Cusip, SecIntCode, 2 as Source_ from ram.dbo.ram_compustat_csvsecurity_map_diffs
+where EXCNTRY = 'USA'
 )
 
--- Append counts to filter out doubles
-, last_entry_by_gvkey_2 as (
-select		A.*,
-			B.Count_
-from		last_entry_by_gvkey A
-join	(	select GVKey, Count(*) as Count_
-			from last_entry_by_gvkey
-			group by GVKey
-		) B
-	on		A.GVKey = B.GVKey
-)
+select * from X
+where GVKey in (6268, 10787)
+
+select * from CSVSecurity
+where GVKey in (6268, 10787)
+
+--where SecIntCode in (5784, 102195)
 
 
-, last_entry_by_gvkey_3 as (
-select		* 
-from		last_entry_by_gvkey_2
-where		(Count_ = 1) 
-	OR		(Count_ = 2 AND TableName = 'CSPITId')
-)
+select * from prc.PrcScChg
+where Code in (51018, 57856)
+
+
+
+select * from X
+where GVKey = 4601
+
+
+select GVKey, Count(*) as Count_ from X
+group by GVKey
+order by Count_ desc
+
+
+
+select top 10 * from ram.dbo.ram_compustat_csvsecurity_map_diffs
+
+
+select * from ram.dbo.ram_compustat_csvsecurity_map_raw
+
+---------------------------------------------------------------------
+-- What happens between PIT table and CSVSecurity_Raw snapshot?
+
+
+-- Locate GOOGLE
+select top 10 * from CSVSecurity
+where TIC = 'GOOG'
+
+
+select * from CSVSecurity
+where GVKey = 160329
+
+select top 10 * from ram.dbo.ram_compustat_csvsecurity_map_raw
+where GVKey = 160329
+
+
+
