a662d240cea46662a5e892c3913be552917cee9d
mitchellsuter <mitchellasuter@gmail.com>
2018-03-19 17:43:07 -0400

    MOD: gcp manager now can download run directories

--- ram/data/data_gcp_manager.py
+++ ram/data/data_gcp_manager.py
@@ -64,6 +64,22 @@ def update_prepped_data_gcp(strategy, version):
         print('\nNo new files to upload for {}/{}\n'.format(strategy, version))
 
 
+def confirm(upload_files):
+
+    print('\nConfirm upload of the following files:')
+    temp = upload_files + ['', '']
+    for i in range(len(temp) / 3):
+        print('{}\t{}\t{}'.format(*temp[(i*3):((i+1)*3)]))
+    print('\n')
+    user_input = raw_input('Continue? Type `1234`: ')
+    if user_input != '1234':
+        return False
+    else:
+        return True
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 def print_trained_model_dirs(strategy):
     gs_model_dirs = get_trained_model_dirs(strategy)
     headline = 'Available Trained Model Directories for {}'.format(strategy)
@@ -104,19 +120,47 @@ def download_trained_models_gcp(strategy, model_dir_name):
     os.system(copy_string)
 
 
-def confirm(upload_files):
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    print('\nConfirm upload of the following files:')
-    temp = upload_files + ['', '']
-    for i in range(len(temp) / 3):
-        print('{}\t{}\t{}'.format(*temp[(i*3):((i+1)*3)]))
+def print_run_dirs(strategy):
+    gs_run_dirs = get_run_dirs(strategy)
+    headline = 'Runs for {}'.format(strategy)
+    _print_line_underscore(headline)
+    for i, model in enumerate(gs_run_dirs):
+        print(" [{}]\t{}".format(i, model))
     print('\n')
-    user_input = raw_input('Continue? Type `1234`: ')
-    if user_input != '1234':
-        return False
-    else:
-        return True
 
+
+def get_run_dirs(strategy):
+    client = storage.Client()
+    bucket = client.get_bucket(config.GCP_STORAGE_BUCKET_NAME)
+    # GCP Directories for implementation
+    gs_files = [x.name for x in bucket.list_blobs()]
+    gs_files = [x for x in gs_files if x.find('simulations') > -1]
+    gs_files = [x for x in gs_files if x.find(strategy) > -1]
+    run_dirs = list(set([x.split('/')[2] for x in gs_files]))
+    run_dirs.sort()
+    return run_dirs
+
+
+def get_run_name(strategy, name_num):
+    run_dirs = get_run_dirs(strategy)
+    if name_num in run_dirs:
+        return name_num
+    else:
+        return run_dirs[int(name_num)]
+
+
+def download_run_gcp(strategy, run_dir_name):
+    gcp_path = "gs://ram_data/simulations/{}/{}".format(
+        strategy, run_dir_name)
+    local_path = os.path.join(config.SIMULATIONS_DATA_DIR, strategy)
+    # TODO: confirm this isn't already a folder
+    copy_string = "gsutil -m cp -r {} {}".format(gcp_path, local_path)
+    os.system(copy_string)
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 if __name__ == '__main__':
 
@@ -133,22 +177,30 @@ if __name__ == '__main__':
         '-sl', '--list_strategies', action='store_true',
         help='List all available local strategies')
     parser.add_argument(
-        '-s', '--strategy_name', type=str,
+        '-s', '--strategy_name', type=str, metavar='',
         help='Strategy to manipulate')
 
     parser.add_argument(
         '-dv', '--list_data_versions', action='store_true',
         help='List all versions of prepped data for a strategy')
     parser.add_argument(
-        '-d', '--data_version', type=str,
+        '-d', '--data_version', type=str, metavar='',
         help='Version to be uploaded')
+
+    parser.add_argument(
+        '-rl', '--list_runs', action='store_true',
+        help='List all directories on GCP platform in '
+        'simulations/--Strategy--')
+    parser.add_argument(
+        '-r', '--run_name', type=str, metavar='',
+        help='Name of run to be downloaded')
 
     parser.add_argument(
         '-ml', '--list_trained_models', action='store_true',
         help='List all directories on GCP platform in '
         'implementation/trained_models')
     parser.add_argument(
-        '-m', '--trained_models_dir', type=str,
+        '-m', '--trained_models_dir', type=str, metavar='',
         help='Directory of models to be downloaded')
 
     parser.add_argument(
@@ -176,7 +228,14 @@ if __name__ == '__main__':
             print '\n[ERROR] - Name or index must be included via `-s`\n'
         else:
             strategy = get_strategy_name(args.strategy_name)
-            print_trained_model_dirs(strategy)
+            print_run_dirs(strategy)
+
+    elif args.list_runs:
+        if args.strategy_name is None:
+            print '\n[ERROR] - Name or index must be included via `-s`\n'
+        else:
+            strategy = get_strategy_name(args.strategy_name)
+            print_run_dirs(strategy)
 
     elif args.strategy_name and args.data_version and args.upload:
         strategy = get_strategy_name(args.strategy_name)
@@ -187,3 +246,8 @@ if __name__ == '__main__':
         strategy = get_strategy_name(args.strategy_name)
         model_dir = get_trained_model_name(strategy, args.trained_models_dir)
         download_trained_models_gcp(strategy, model_dir)
+
+    elif args.strategy_name and args.run_name and args.download:
+        strategy = get_strategy_name(args.strategy_name)
+        run_dir = get_run_name(strategy, args.run_name)
+        download_run_gcp(strategy, run_dir)
