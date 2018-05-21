4f84686f3906b2739b309bf3e2288cae73def758
Mitchell Suter <mitchellsuter@users.noreply.github.com>
2018-05-21 12:50:49 -0400

    Update daily_mapping.bat

--- data/ram_sql_tables/gvkey_mapping/daily_mapping.bat
+++ data/ram_sql_tables/gvkey_mapping/daily_mapping.bat
@@ -6,7 +6,7 @@ set SQLCMD="%SQLCMDPATH%\sqlcmd" -S 192.168.2.8 -d ram -U ramuser -P 183madison 
 
 set SCRIPTS=%GITHUB%\ram\data\ram_sql_tables\gvkey_mapping\
 
-set OUTPUT=%DATA%\ram\data\gvkey_mapping2\
+set OUTPUT=%DATA%\ram\data\gvkey_mapping\
 
 set dateprefix=%date:~10,4%%date:~4,2%%date:~7,2%
 
