ad0a1b939129f2db371ac785b534554464cd4f27
ryanoley <ryan@roundaboutam.com>
2018-06-19 13:56:05 -0400

    MOD: Add in spacing and process complete prints

--- ram/strategy/statarb/implementation/StatArb_Application.bat
+++ ram/strategy/statarb/implementation/StatArb_Application.bat
@@ -6,7 +6,9 @@ SETLOCAL ENABLEEXTENSIONS
 set RAMEX_SCRIPT=%GITHUB%\ramex\ramex\scripts
 set RAMEX_APP=%GITHUB%\ramex\ramex\application
 set STATARB_IMP=%GITHUB%\ram\ram\strategy\statarb\implementation
+
 CLS
+
 :MENU
 ECHO.
 ECHO.
@@ -21,7 +23,7 @@ ECHO 1.  Prep Data: 10:15 am or later. Must be run first every day
 ECHO         and the output must be verified before running #2.
 ECHO 2.  Run Trade: 3:40 pm or before.  Click Start ZMQ in Trader
 ECHO         Engine and then export live prices in live_pricing
-ECHO         sheet.  When prompted, press Enter if these are complete.
+ECHO         sheet.  When prompted, press ENTER if these are complete.
 ECHO 3.  Reconcile Executed Trades. 4:00 pm after all trading. You
 ECHO         must click Export Trades in Trader Engine first.                                                  --
 ECHO 4.  Reconcile Portfolio. 4:30 pm after Fund Manager is updated
@@ -44,6 +46,7 @@ ECHO.
 ECHO --PREPARING DATA--
 :: Start RAMEX Server
 python %STATARB_IMP%\prep_data.py
+ECHO --PREP DATA COMPLETE--
 GOTO MENU
 
 
@@ -52,9 +55,10 @@ ECHO.
 ECHO.
 ECHO --GETTING LIVE ALLOCATIONS--
 :: Start RAMEX Server
-start python %RAMEX_APP%\server.py
+start python %RAMEX_APP%\server.py -p
 :: Run get live allocations script
 python %STATARB_IMP%\get_live_allocations.py
+ECHO --LIVE ALLOCATIONS COMPLETE--
 GOTO MENU
 
 
@@ -64,6 +68,7 @@ ECHO.
 ECHO --RECONCILING TRADES--
 :: Match Stat Arb Trades with Trader Engine Executions
 python %RAMEX_SCRIPT%\trade_reconciliation.py
+ECHO --TRADE RECONCILIATION COMPLETE--
 GOTO MENU
 
 
@@ -73,6 +78,7 @@ ECHO.
 ECHO --RECONCILING WITH FUND MANAGER--
 :: Reconcile Stat Arb Trades with Trader Engine Executions
 python %RAMEX_SCRIPT%\portfolio_reconciliation.py -u -w -v
+ECHO --PORTFOLIO RECONCILIATION COMPLETE--
 GOTO MENU
 
 
