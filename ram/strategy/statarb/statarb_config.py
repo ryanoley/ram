0b6dbf32b81eacc39bf0791189f91a3cf95086b7
mitchellsuter <mitchellasuter@gmail.com>
2018-10-01 13:29:41 -0400

    MOD: updated parameters for stat arb imp

--- ram/strategy/statarb/statarb_config.py
+++ ram/strategy/statarb/statarb_config.py
@@ -3,7 +3,7 @@
 # ~~~~~~~~~~~~~~~~~~~~~~#
 
 # From Model Selection
-parameter_json = 'current_params_CombinationSearch_20180904110521.json'
+parameter_json = 'current_params_CombinationSearch_20181001170914.json'
 
 # From Implementation Training
 trained_models_dir_name = 'models_0043'
