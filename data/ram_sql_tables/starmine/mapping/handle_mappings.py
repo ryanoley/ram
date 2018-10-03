f142456819578ca64f07cf484b4141e0770ec18a
mitchellsuter <mitchellasuter@gmail.com>
2018-10-03 17:22:59 -0400

    MOD: email will be sent for problem mappings

--- data/ram_sql_tables/starmine/mapping/handle_mappings.py
+++ data/ram_sql_tables/starmine/mapping/handle_mappings.py
@@ -1,8 +1,39 @@
 import os
 import pandas as pd
 from gearbox import read_sql
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
 
 from ram.data.data_handler_sql import DataHandlerSQL
+
+
+###########
+#  EMAIL  #
+###########
+
+def send_email(msg_body, subject, toaddr="analysts@roundaboutam.com"):
+    """
+    Send an email with msg in the body from the notifications@roundaboutam.com
+    address.
+    """
+    fromaddr = "notifications@roundaboutam.com"
+    passwd = "ramalerts"
+
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = subject
+
+    body = msg_body
+    msg.attach(MIMEText(body, 'plain'))
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, passwd)
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
+    return
 
 
 #################
@@ -37,6 +68,9 @@ problems = problems[~problems.SecCode.isin(handled.SecCode)]
 
 if len(problems) > 0:
     problems.to_csv(os.path.join(dpath, 'PROBLEMS.csv'), index=None)
+    msg = "There are {} problem mappings for Starmine.".format(len(problems))
+    msg += "\nLocate securities in DATA/ram/data/starmine/PROBLEMS.csv"
+    send_email(msg, subject='[DATABASE] Starmine Mapping Problem')
 
 
 ##################################
