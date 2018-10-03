47a7dd8d6545da45b46157881f81cd89b2f7915d
mitchellsuter <mitchellasuter@gmail.com>
2018-10-03 16:59:17 -0400

    MOD: working mapping

--- data/ram_sql_tables/starmine/mapping/handle_mappings.py
+++ data/ram_sql_tables/starmine/mapping/handle_mappings.py
@@ -1,6 +1,5 @@
 import os
 import pandas as pd
-import datetime as dt
 from gearbox import read_sql
 
 from ram.data.data_handler_sql import DataHandlerSQL
@@ -15,14 +14,18 @@ fpath = os.path.join(os.getenv('GITHUB'), 'ram', 'data', 'ram_sql_tables',
                      'starmine', 'mapping', 'handled_mappings.csv')
 
 handled = pd.read_csv(fpath)
+handled = handled[['SecCode', 'SecId', 'StartDate', 'EndDate']]
 
 # Get todays problem list
 dpath = os.path.join(os.getenv('DATA'), 'ram', 'data', 'starmine')
 
-file_name = [x for x in os.listdir(dpath) if
-             x.find(dt.date.today().strftime('%Y%m%d')) > -1][0]
+# First, delete problem file
+try:
+    os.remove(os.path.join(dpath, 'PROBLEMS.csv'))
+except:
+    pass
 
-problems = read_sql(os.path.join(dpath, file_name))
+problems = read_sql(os.path.join(dpath, max(os.listdir(dpath))))
 
 
 ########################
@@ -32,7 +35,8 @@ problems = read_sql(os.path.join(dpath, file_name))
 # Extract SecCodes that have been handled
 problems = problems[~problems.SecCode.isin(handled.SecCode)]
 
-problems.to_csv(os.path.join(dpath, 'PROBLEMS.csv'), index=None)
+if len(problems) > 0:
+    problems.to_csv(os.path.join(dpath, 'PROBLEMS.csv'), index=None)
 
 
 ##################################
