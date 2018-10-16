beabdc0deda766e9df5ee510d8e5eaa9fecc8b34
mitchellsuter <mitchellasuter@gmail.com>
2018-10-16 11:50:27 -0400

    MOD: config for new models

--- ram/strategy/statarb/statarb_config.py
+++ ram/strategy/statarb/statarb_config.py
@@ -6,4 +6,4 @@
 parameter_json = 'current_params_CombinationSearch_20181015170123.json'
 
 # From Implementation Training
-trained_models_dir_name = 'models_0046'
+trained_models_dir_name = 'models_0047'
