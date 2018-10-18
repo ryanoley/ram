9435c532a7a8e22b729da445cc239e284932bbdf
mitchellsuter <mitchellasuter@gmail.com>
2018-10-18 10:32:23 -0400

    DEL: data/ernpead direct moved to ernpead repo

--- data/ern_pead/reconciliation/seccode_cusip_map.sql
+++
@@ -1,5 +0,0 @@
-SET NOCOUNT ON
-
-select distinct SecCode, Ticker, Cusip, StartDate, EndDate from ram.dbo.ram_master_ids
-where EndDate >= dateadd(day, -180, getdate())
-and SecCode in (select distinct SecCode from ram.dbo.ram_equity_pricing_research)
